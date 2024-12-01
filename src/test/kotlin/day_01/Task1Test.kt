2454ae84f89271a83fb9766320fb914e1fef31aa
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-01 19:12:10 +0200

    refactoring

--- src/test/kotlin/day_01/Task1Test.kt
+++ src/test/kotlin/day_01/Task1Test.kt
@@ -1,4 +1,4 @@
-package day1
+package day_01
 
 import com.akryvtsun.day1.Task1
 import org.junit.jupiter.api.Test
@@ -18,7 +18,7 @@ class Task1Test {
     fun `should successfully solve the real task 1`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
-        java.io.File("src/test/resources/day1/TaskData.txt").forEachLine { line ->
+        java.io.File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
             val parts = line.split("   ")
             list1.add(parts[0].toLong())
             list2.add(parts[1].toLong())
--- src/test/kotlin/day_01/Task2Test.kt
+++ src/test/kotlin/day_01/Task2Test.kt
@@ -1,4 +1,4 @@
-package day1
+package day_01
 
 import com.akryvtsun.day1.Task2
 import org.junit.jupiter.api.Test
@@ -18,7 +18,7 @@ class Task2Test {
     fun `should successfully solve the real task 2`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
-        java.io.File("src/test/resources/day1/TaskData.txt").forEachLine { line ->
+        java.io.File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
             val parts = line.split("   ")
             list1.add(parts[0].toLong())
             list2.add(parts[1].toLong())

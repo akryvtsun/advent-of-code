8761e5f34becfea52be618af66479a60ce684fc4
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-01 17:34:06 +0200

    initial task 2 solving

---
+++ src/main/kotlin/day1/Task2.kt
@@ -0,0 +1,11 @@
+package com.akryvtsun.day1
+
+
+class Task2 {
+    companion object {
+
+        fun solve(list1: List<Long>, list2: List<Long>): Long {
+            return 0
+        }
+    }
+}
--- src/test/kotlin/day1/Task2Test.kt
+++ src/test/kotlin/day1/Task2Test.kt
@@ -1,21 +1,21 @@
 package day1
 
-import com.akryvtsun.day1.Task1
+import com.akryvtsun.day1.Task2
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class Task1Test {
+class Task2Test {
 
     @Test
-    fun `should successfully pass task 1 example`() {
+    fun `should successfully pass task 2 example`() {
         val list1: List<Long> = listOf(3, 4, 2, 1, 3, 3)
         val list2: List<Long> = listOf(4, 3, 5, 3, 9, 3)
 
-        assertEquals(11, Task1.solve(list1, list2))
+        assertEquals(31, Task2.solve(list1, list2))
     }
 
     @Test
-    fun `should successfully solve the real task 1`() {
+    fun `should successfully solve the real task 2`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
         java.io.File("src/test/resources/day1/TaskData.txt").forEachLine { line ->
@@ -23,6 +23,6 @@ class Task1Test {
             list1.add(parts[0].toLong())
             list2.add(parts[1].toLong())
         }
-        println("Task 1 solution: ${Task1.solve(list1, list2)}")
+        println("Task 1 solving: ${Task2.solve(list1, list2)}")
     }
 }

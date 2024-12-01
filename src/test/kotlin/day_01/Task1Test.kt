3056a03f46e9a4a3bf31336a2512ac5ab4a5549b
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-01 19:20:31 +0200

    refactoring

--- src/test/kotlin/day_01/Task1Test.kt
+++ src/test/kotlin/day_01/Task1Test.kt
@@ -2,6 +2,7 @@ package day_01
 
 import com.akryvtsun.day1.Task1
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 class Task1Test {
@@ -18,7 +19,7 @@ class Task1Test {
     fun `should successfully solve the real task 1`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
-        java.io.File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
+        File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
             val parts = line.split("   ")
             list1.add(parts[0].toLong())
             list2.add(parts[1].toLong())
--- src/test/kotlin/day_01/Task2Test.kt
+++ src/test/kotlin/day_01/Task2Test.kt
@@ -2,6 +2,7 @@ package day_01
 
 import com.akryvtsun.day1.Task2
 import org.junit.jupiter.api.Test
+import java.io.File
 import kotlin.test.assertEquals
 
 class Task2Test {
@@ -18,7 +19,7 @@ class Task2Test {
     fun `should successfully solve the real task 2`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
-        java.io.File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
+        File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
             val parts = line.split("   ")
             list1.add(parts[0].toLong())
             list2.add(parts[1].toLong())

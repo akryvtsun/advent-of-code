23057d72f184df208c39b669ace4a8167fa9ad85
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-02 11:36:35 +0200

    initial task 2 impl

--- src/main/kotlin/day_02/Task2.kt
+++ src/main/kotlin/day_02/Task2.kt
@@ -12,14 +12,49 @@ class Task2 {
         }
 
         private fun isSafe(path: List<Int>): Boolean {
-            val steps =  path.zipWithNext { first, second -> second - first }
-            val unSafeCount = steps.count { !isSafeStep(it) }
-            val isMonotonic = steps.zipWithNext().all { haveSameSign(it.first, it.second) }
-            return isMonotonic && unSafeCount <= 1
+            val mpath = path.toMutableList()
+            for (i in (1 until mpath.size-1)) {
+                // check left step
+                if (!isSafeStep(mpath[i] - mpath[i - 1])) {
+                    val v = mpath.removeAt(i - 1)
+                    if (isSafeCheck(mpath)) return true
+                    mpath.add(i - 1, v)
+                    mpath.removeAt(i)
+                    if (isSafeCheck(mpath)) return true
+                    return false
+                }
+                // check right step
+                if (!isSafeStep(mpath[i + 1] - mpath[i])) {
+                    val v = mpath.removeAt(i)
+                    if (isSafeCheck(mpath)) return true
+                    mpath.add(i, v)
+                    mpath.removeAt(i + 1)
+                    if (isSafeCheck(mpath)) return true
+                    return false
+                }
+                // check monotonic
+                if (!haveSameSign(mpath[i] - mpath[i - 1], mpath[i + 1] - mpath[i])) {
+                    val v = mpath.removeAt(i)
+                    if (isSafeCheck(mpath)) return true
+                    mpath.add(i, v)
+                    mpath.removeAt(i + 1)
+                    if (isSafeCheck(mpath)) return true
+                    return false
+                }
+            }
+            return true
         }
 
         private fun haveSameSign(first: Int, second: Int) = first.sign == second.sign
 
         private fun isSafeStep(step: Int) = abs(step) in 1..3
+
+        private fun isSafeCheck(path: List<Int>): Boolean {
+            return path
+                .zipWithNext()
+                .map { it.second - it.first }
+                .zipWithNext()
+                .all { isSafeStep(it.first) && haveSameSign(it.first, it.second) && isSafeStep(it.second) }
+        }
     }
 }

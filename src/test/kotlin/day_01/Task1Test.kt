cbe753813499988fc51df8fe172440efbd2b85ca
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-02 11:56:42 +0200

    refactoring

--- src/test/kotlin/day_01/Task1Test.kt
+++ src/test/kotlin/day_01/Task1Test.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 class Task1Test {
 
     @Test
-    fun `should successfully pass task 1 example`() {
+    fun `should successfully pass task example`() {
         val list1: List<Long> = listOf(3, 4, 2, 1, 3, 3)
         val list2: List<Long> = listOf(4, 3, 5, 3, 9, 3)
 
@@ -15,7 +15,7 @@ class Task1Test {
     }
 
     @Test
-    fun `should successfully solve the real task 1`() {
+    fun `should successfully solve real task`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
         File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->
--- src/test/kotlin/day_01/Task2Test.kt
+++ src/test/kotlin/day_01/Task2Test.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 class Task2Test {
 
     @Test
-    fun `should successfully pass task 2 example`() {
+    fun `should successfully pass task example`() {
         val list1: List<Long> = listOf(3, 4, 2, 1, 3, 3)
         val list2: List<Long> = listOf(4, 3, 5, 3, 9, 3)
 
@@ -15,7 +15,7 @@ class Task2Test {
     }
 
     @Test
-    fun `should successfully solve the real task 2`() {
+    fun `should successfully solve the real task`() {
         val list1 = mutableListOf<Long>()
         val list2 = mutableListOf<Long>()
         File("src/test/resources/day_01/TaskData.txt").forEachLine { line ->

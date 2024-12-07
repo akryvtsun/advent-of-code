ff94e73e1dbc807a12035cd07c639d6b44ed3ca4
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-07 08:07:12 +0200

    task 2 complete

---
+++ src/main/kotlin/day_07/Task2.kt
@@ -0,0 +1,49 @@
+package day_07
+
+class Task2 {
+
+    enum class Operation{
+        PLUS {
+            override fun apply(arg1: Long, arg2: Long) = arg1 + arg2
+        },
+        MULTIPLY {
+            override fun apply(arg1: Long, arg2: Long) = arg1 * arg2
+        },
+        CONCAT {
+            override fun apply(arg1: Long, arg2: Long) = (arg1.toString() + arg2.toString()).toLong()
+        };
+
+        abstract fun apply(arg1: Long, arg2: Long): Long
+    }
+
+    companion object {
+
+        fun solve(data: List<Equation>): Long {
+            return data
+                .filter { isTrue(it) }
+                .sumOf { it.test }
+        }
+
+        private fun isTrue(equation: Equation): Boolean {
+            val opsPermutations = generateOps(equation.nums.size-1)
+            return opsPermutations.any { operations ->
+                    equation.nums.reduceIndexed {
+                        index, acc, value -> operations[index-1].apply(acc, value)
+                    } == equation.test
+                }
+        }
+
+        private fun generateOps(side: Int) : Sequence<List<Operation>> = sequence {
+            for (operation in Operation.entries) {
+                if (side == 1) {
+                    yield(listOf(operation))
+                }
+                else {
+                    for (permutation in generateOps(side-1)) {
+                        yield(listOf(operation) + permutation)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/day_07/Task2Test.kt
@@ -0,0 +1,41 @@
+package day_07
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Task2Test {
+
+    @Test
+    fun `should successfully pass task example`() {
+        val input =  """
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
+        """.trimIndent()
+        val data = transform(input)
+        assertEquals(11387, Task2.solve(data))
+    }
+
+    private fun transform(input: String): List<Equation> {
+        return input.split("\n")
+            .map { line ->
+                val test = line.substringBefore(':').toLong()
+                val nums = line.substringAfter(": ").split(" ").map { it.toLong() }
+                Equation(test, nums)
+            }
+    }
+
+    @Test
+    fun `should successfully solve the real task`() {
+        val input = File("src/test/resources/day_07/TaskData.txt").readText()
+        val data = transform(input)
+        assertEquals(106016735664498, Task2.solve(data))
+    }
+}

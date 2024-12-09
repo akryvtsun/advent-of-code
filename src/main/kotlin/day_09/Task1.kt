51b3c06b2361580288fbdc4a598da850626cc530
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-09 09:24:01 +0200

    Task 1 almost complete

--- src/main/kotlin/day_09/Task1.kt
+++ src/main/kotlin/day_09/Task1.kt
@@ -4,8 +4,49 @@ class Task1 {
 
     companion object {
 
-        fun solve(data: String): Long {
-            return 0
+        fun solve(input: String): Long {
+            val viz = makeViz(input)
+            //println(viz)
+            val packRez = pack(viz)
+            //println(packRez)
+            val checksum = calcSum(packRez)
+            return checksum
+        }
+
+        fun makeViz(data: String): String {
+            var viz = StringBuilder()
+            var id = 0
+            data.forEachIndexed { i, c ->
+                val type = if (isFile(i)) (id++).toString() else "."
+                val size = c.digitToInt()
+                viz.append(type.repeat(size))
+            }
+            return viz.toString()
+        }
+
+        private fun isFile(pos: Int): Boolean = pos % 2 == 0
+
+        fun pack(data: String): String {
+            var begin = 0
+            var end = data.length-1
+            val buf = StringBuilder(data)
+            while (true) {
+                while (buf[begin] in '0'..'9') begin++
+                while (buf[end] == '.') end--
+                if (begin >= end) break
+                buf[begin] = buf[end]
+                buf[end] = '.'
+            }
+            return buf.toString()
+        }
+
+        fun calcSum(data: String): Long {
+            var sum = 0L
+            data.forEachIndexed { i, c ->
+                val digit = if (c == '.') 0 else c.digitToInt()
+                sum += i * digit
+            }
+            return sum
         }
     }
 }

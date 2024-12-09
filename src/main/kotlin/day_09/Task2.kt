347d9ed8950d7fd8f50b1fe3845acd1765144802
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-09 14:57:07 +0200

    Task 2 draft

--- src/main/kotlin/day_09/Task2.kt
+++ src/main/kotlin/day_09/Task2.kt
@@ -2,49 +2,80 @@ package day_09
 
 class Task2 {
 
-    companion object {
+    sealed class FsBlock(open val size: Int)
+    data class File(val id: Int, override val size: Int): FsBlock(size)
+    data class Space(override val size: Int): FsBlock(size)
 
-        const val SPACE = -1
+    companion object {
 
         fun solve(input: String): Long {
             val viz = makeViz(input)
+            println(viz)
             val packRez = pack(viz)
-            val checksum = calcSum(packRez)
-            return checksum
+            println(packRez)
+            return calcSum(packRez)
         }
 
-        fun makeViz(data: String): List<Int> {
-            var viz = mutableListOf<Int>()
+        fun makeViz(data: String): List<FsBlock> {
+            var viz = mutableListOf<FsBlock>()
             var id = 0
             data.forEachIndexed { i, c ->
-                val type = if (isFile(i)) id++ else SPACE
                 val size = c.digitToInt()
-                viz += List(size) { type }
+                val block = if (isFile(i)) File(id++, size) else Space(size)
+                viz += block
             }
             return viz
         }
 
         private fun isFile(pos: Int): Boolean = pos % 2 == 0
 
-        fun pack(data: List<Int>): List<Int> {
-            var begin = 0
-            var end = data.size-1
+        fun pack(data: List<FsBlock>): List<FsBlock> {
             val buf = data.toMutableList()
-            while (true) {
-                while (buf[begin] != SPACE) begin++
-                while (buf[end] == SPACE) end--
-                if (begin >= end) break
-                buf[begin] = buf[end]
-                buf[end] = SPACE
+            var right = data.size-1
+            while (right > 0) {
+                while (buf[right] is Space) right--
+                val rightFile = buf[right]
+
+                var left = 0
+                while (left < right) {
+                    while (buf[left] !is Space) left++
+                    val leftSpace = buf[left]
+
+                    if (leftSpace.size >= rightFile.size) {
+                        buf[left++] = rightFile
+                        val newSpaceSize = leftSpace.size - rightFile.size
+                        if (newSpaceSize > 0) {
+                            buf.putSpace(left, newSpaceSize)
+                        }
+                        buf.putSpace(right--, rightFile.size)
+                        break
+                    }
+                    left++
+                }
             }
             return buf
         }
 
-        fun calcSum(data: List<Int>): Long {
+        fun MutableList<FsBlock>.putSpace(pos: Int, size: Int) {
+            if (this[pos] is Space) {
+                this[pos] = Space(this[pos].size + size)
+            } else {
+                if (this[pos-1] is Space) {
+                    this[pos-1] = Space(this[pos-1].size + size)
+                    this.removeAt()
+                }
+                this.add(pos, Space(size))
+            }
+        }
+
+        fun calcSum(data: List<FsBlock>): Long {
             var sum = 0L
-            data.forEachIndexed { i, num ->
-                val value = if (num == SPACE) 0 else num
-                sum += i * value
+            var left = 0
+            data.forEach { block ->
+                for (i in 0 until block.size) {
+                    sum += (left+i) * if (block is File) block.id else 0
+                }
+                left += block.size
             }
             return sum
         }

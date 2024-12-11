93c51cb10dacc4cccc577ce93359ea62481d42cd
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-11 09:18:38 +0200

    refactoring

--- src/main/kotlin/day_11/Task1.kt
+++ src/main/kotlin/day_11/Task1.kt
@@ -1,8 +1,5 @@
 package day_11
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-
 class Task1 {
 
     companion object {
@@ -10,6 +7,7 @@ class Task1 {
         fun solve(stones: List<Long>, blinks: Int): Int {
             var state = stones.toMutableList()
             repeat(blinks) {
+                println("$it. size=${state.size}")
                 state = blink(state)
             }
             return state.size
@@ -17,23 +15,32 @@ class Task1 {
 
         fun blink(stones: MutableList<Long>): MutableList<Long> {
             var i = 0
-            runBlocking {
-                while (i < stones.size) {
-                    if (stones[i] == 0L) {
-                        stones[i] = 1
-                    } else {
-                        val strStone = stones[i].toString()
-                        if (strStone.length % 2 == 0) {
-                            val first = async { strStone.substring(0..<strStone.length / 2).toLong() }
-                            val second = async { strStone.substring(strStone.length / 2..<strStone.length).toLong() }
-                            stones[i++] = first.await()
-                            stones.add(i, second.await())
-                        } else {
-                            stones[i] = stones[i] * 2024
-                        }
+            while (i < stones.size) {
+                if (stones[i] == 0L) {
+                    stones[i] = 1
+                } else {
+                    var num = stones[i]
+                    var count = 0
+                    var mul = 1L
+                    do {
+                        mul *= 10
+                        num /= 10
+                        count++
+                    } while (num > mul)
+                    val left = num
+                    val right = stones[i] % mul
+                    while (num > 0) {
+                        num /= 10
+                        count++
                     }
-                    i++
+                    if (count % 2 == 0) {
+                        stones[i++] = left
+                        stones.add(i, right)
+                    } else {
+                        stones[i] = stones[i] * 2024
+                    }
                 }
+                i++
             }
             return stones
         }

f3b6e3242eb7ce69eb289a74f023785b5ac9e1d4
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-11 10:30:08 +0200

    rollback

--- src/main/kotlin/day_11/Task1.kt
+++ src/main/kotlin/day_11/Task1.kt
@@ -1,10 +1,5 @@
 package day_11
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.ConcurrentHashMap
-
 class Task1 {
 
     companion object {
@@ -19,43 +14,35 @@ class Task1 {
         }
 
         fun blink(stones: MutableList<Long>): MutableList<Long> {
-            val toAdd = ConcurrentHashMap<Int, Long>()
-            runBlocking(Dispatchers.Default) {
-                for (i in stones.indices) {
-                    launch {
-                        if (stones[i] == 0L) {
-                            stones[i] = 1
-                        } else {
-                            var num = stones[i]
-                            var count = 0
-                            var mul = 1L
-                            // find middle of number
-                            do {
-                                mul *= 10
-                                num /= 10
-                                count++
-                            } while (num > mul)
-                            val left = num
-                            val right = stones[i] % mul
-                            // define number count of digits
-                            while (num > 0) {
-                                num /= 10
-                                count++
-                            }
-                            if (count % 2 == 0) {
-                                stones[i] = left
-                                toAdd.put(i + 1, right)
-                            } else {
-                                stones[i] = stones[i] * 2024
-                            }
-                        }
+            var i = 0
+            while (i < stones.size) {
+                if (stones[i] == 0L) {
+                    stones[i] = 1
+                } else {
+                    var num = stones[i]
+                    var count = 0
+                    var mul = 1L
+                    // find middle of number
+                    do {
+                        mul *= 10
+                        num /= 10
+                        count++
+                    } while (num > mul)
+                    val left = num
+                    val right = stones[i] % mul
+                    // define number count of digits
+                    while (num > 0) {
+                        num /= 10
+                        count++
+                    }
+                    if (count % 2 == 0) {
+                        stones[i++] = left
+                        stones.add(i, right)
+                    } else {
+                        stones[i] = stones[i] * 2024
                     }
                 }
-            }
-            var shift = 0
-            for (i in toAdd.keys.sorted()) {
-                stones.add(i + shift, toAdd.get(i)!!)
-                shift++
+                i++
             }
             return stones
         }

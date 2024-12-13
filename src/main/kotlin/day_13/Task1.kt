65d3df6332dd46b8b5a02e0fb970ea4feb9f2b36
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-13 08:06:17 +0200

    refactoring

--- src/main/kotlin/day_13/Task1.kt
+++ src/main/kotlin/day_13/Task1.kt
@@ -15,7 +15,8 @@ class Task1 {
             with(slot) {
                 val b = (dA.x * prize.y - dA.y * prize.x) / (dA.x * dB.y - dA.y * dB.x)
                 val a = (prize.y - dB.y * b) / dA.y
-                return if (dA.y * a + dB.y * b == prize.y && dA.x * a + dB.x * b == prize.x) Pair(a, b) else Pair(0, 0)
+                val check = dA.y * a + dB.y * b == prize.y && dA.x * a + dB.x * b == prize.x
+                return if (check) Pair(a, b) else Pair(0, 0)
             }
         }
     }

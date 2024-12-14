fc357dff782bb880c0b83f962f3d73bf53164f1d
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-14 09:15:05 +0200

    task 1 init

--- src/main/kotlin/day_14/Task1.kt
+++ src/main/kotlin/day_14/Task1.kt
@@ -5,7 +5,52 @@ class Task1 {
     companion object {
 
         fun solve(height: Int, width: Int, robots: List<Robot>): Long {
-            return 0
+
+            fun move(unit: Robot, rounds: Int): Point {
+                var curY = unit.coord.y
+                var curX = unit.coord.x
+                repeat(rounds) {
+                    curY = (curY + unit.delta.y) % height
+                    if (curY < height) curY += height
+                    curX = (curX + unit.delta.x) % width
+                    if (curX < width) curX += width
+                }
+                return Point(curY, curX)
+            }
+
+            fun quadrant(c: Point): Int {
+                val y0 = height/2+1
+                val x0 = width/2+1
+                return if (c.y < y0) {
+                    if (c.x < x0) {
+                        1
+                    } else if (c.x > x0) {
+                        2
+                    }
+                    else {
+                        -1
+                    }
+                } else if (c.y > y0) {
+                    if (c.x < x0) {
+                        3
+                    } else if (c.x > x0) {
+                        4
+                    }
+                    else {
+                        -1
+                    }
+                }
+                else {
+                    -1
+                }
+            }
+
+            val newCoords = robots.map { move(it, 100) }
+            val securityMap = newCoords.map { it to quadrant(it) }.groupBy({ it.second }, { it.first })
+            return securityMap
+                .filter { it.key != -1 }
+                .map { it.value.size }
+                .fold(1) { acc, num -> acc * num }
         }
     }
 }

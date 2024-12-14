62cd7dce79d5ccbcbccab76687299f969bd63d44
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-14 09:28:43 +0200

    task 1 complete

--- src/main/kotlin/day_14/Task1.kt
+++ src/main/kotlin/day_14/Task1.kt
@@ -4,23 +4,26 @@ class Task1 {
 
     companion object {
 
+        private fun roundMove(value: Int, delta: Int, border: Int): Int {
+            val newValue = (value + delta) % border
+            return if (newValue < 0) border + newValue else newValue
+        }
+
         fun solve(height: Int, width: Int, robots: List<Robot>): Long {
 
             fun move(unit: Robot, rounds: Int): Point {
                 var curY = unit.coord.y
                 var curX = unit.coord.x
                 repeat(rounds) {
-                    curY = (curY + unit.delta.y) % height
-                    if (curY < height) curY += height
-                    curX = (curX + unit.delta.x) % width
-                    if (curX < width) curX += width
+                    curY = roundMove(curY, unit.delta.y, height)
+                    curX = roundMove(curX, unit.delta.x, width)
                 }
                 return Point(curY, curX)
             }
 
             fun quadrant(c: Point): Int {
-                val y0 = height/2+1
-                val x0 = width/2+1
+                val y0 = height/2
+                val x0 = width/2
                 return if (c.y < y0) {
                     if (c.x < x0) {
                         1
@@ -28,7 +31,7 @@ class Task1 {
                         2
                     }
                     else {
-                        -1
+                        0
                     }
                 } else if (c.y > y0) {
                     if (c.x < x0) {
@@ -37,18 +40,18 @@ class Task1 {
                         4
                     }
                     else {
-                        -1
+                        0
                     }
                 }
                 else {
-                    -1
+                    0
                 }
             }
 
             val newCoords = robots.map { move(it, 100) }
             val securityMap = newCoords.map { it to quadrant(it) }.groupBy({ it.second }, { it.first })
             return securityMap
-                .filter { it.key != -1 }
+                .filter { it.key != 0 }
                 .map { it.value.size }
                 .fold(1) { acc, num -> acc * num }
         }

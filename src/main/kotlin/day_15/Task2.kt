25b2aef01e6177c9de07f5b6585e2a760c7dacf1
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-15 09:51:12 +0200

    task 2 init

--- src/main/kotlin/day_15/Task2.kt
+++ src/main/kotlin/day_15/Task2.kt
@@ -2,21 +2,32 @@ package day_15
 
 class Task2 {
 
+    data class Box(val left: Point, val right: Point) {
+        operator fun contains(point: Point): Boolean {
+            return point.y == left.y && (point.x in left.x..right.x)
+        }
+
+        operator fun plus(point: Point): Box {
+            return Box(left + point, right + point)
+        }
+    }
+
     class Warehouse(map: String) {
 
         val obstacles = mutableListOf<Point>()
-        val boxes = mutableListOf<Point>()
+        val boxes = mutableListOf<Box>()
         var robot: Point? = null
 
         init {
             val data = map.lines()
             for (y in data.indices) {
                 for (x in data.first().indices) {
-                    val point = Point(y, x)
+                    val left = Point(y, x*2)
+                    val right = Point(y, x*2+1)
                     when (data[y][x]) {
-                        '#' -> obstacles += point
-                        'O' -> boxes += point
-                        '@' -> robot = point
+                        '#' -> obstacles += listOf(left, right)
+                        'O' -> boxes += Box(left, right)
+                        '@' -> robot = left
                     }
                 }
             }
@@ -34,22 +45,27 @@ class Task2 {
                 if (next in obstacles) {
                     continue    // skip move
                 }
-                if (next in boxes) {
-                    var nextBox = next
-                    do {
-                        nextBox += cmd.delta
-                    } while (nextBox in boxes)
-                    if (nextBox in obstacles) {
+                val b = boxes.firstOrNull { next in it }
+                if (b != null) {
+                    val bSet = mutableSetOf(b)
+                    var nextPos = next
+                    while (true) {
+                        nextPos += cmd.delta
+                        val nb = boxes.firstOrNull { nextPos in it }
+                        if (nb == null) break
+                        bSet += nb
+                    }
+                    if (nextPos in obstacles) {
                         continue // boxes close to obstacle in cmd direction - skip move
                     }
-                    else {  // move first box in chain to finishing space
-                        boxes -= next
-                        boxes += nextBox
+                    else {  // move boxes set
+                        boxes -= bSet
+                        boxes += bSet.map { it + cmd.delta }
                     }
                 }
                 cur = next // move robot to new position in cmd direction
             }
-            return boxes.sumOf { it.y * 100 + it.x }
+            return boxes.sumOf { it.left.y * 100 + it.left.x }
         }
     }
 }

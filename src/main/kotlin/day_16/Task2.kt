7fb3b871c0b6fe8a660d9685ee675029d54aec39
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-16 20:54:55 +0200

    task 2 init

--- src/main/kotlin/day_16/Task2.kt
+++ src/main/kotlin/day_16/Task2.kt
@@ -10,35 +10,44 @@ class Task2 {
 
         fun solve(config: Labyrinth): Int {
             val initPos = Step(config.start, Direction.RIGHT)
-            val unvisited = PriorityQueue<Pair<Step, Int>>(compareBy { it.second })
-            unvisited.add(initPos to 0)
-            val visited = mutableSetOf<Step>()
+            val unvisited = PriorityQueue<Pair<List<Step>, Int>>(compareBy { it.second })
+            unvisited.add(listOf(initPos) to 0)
+            var best: Int? = null
+            val res = mutableSetOf<Point>()
             while (unvisited.isNotEmpty()) {
-                val path = unvisited.poll().also { visited += it.first }
-                val curStep = path.first
+                val path = unvisited.poll()
+                val curStep = path.first.last()
                 if (curStep.pos == config.end) {
-                    return path.second
+                    if (best == null) {
+                        best = path.second
+                        res += path.first.map { it.pos }
+                    } else if (path.second == best) {
+                        res += path.first.map { it.pos }
+                    }
                 } else {
-                    unvisited += listOf(-1, 0, 1)
-                        .map {
-                            var newPos = curStep.pos
-                            val newDir =
-                                Direction.entries[((curStep.dir.ordinal + it) + Direction.entries.size) % Direction.entries.size]
-                            var newCount = path.second
-                            if (it == 0) {
-                                newPos += newDir.delta
-                                newCount += 1
-                            } else {
-                                newCount += 1000
+                    if (best == null || path.second < best) {
+                        unvisited += listOf(-1, 0, 1)
+                            .map {
+                                var newPos = curStep.pos
+                                val newDir =
+                                    Direction.entries[((curStep.dir.ordinal + it) + Direction.entries.size) % Direction.entries.size]
+                                var newCount = path.second
+                                if (it == 0) {
+                                    newPos += newDir.delta
+                                    newCount += 1
+                                } else {
+                                    newCount += 1000
+                                }
+                                val newStep = Step(newPos, newDir)
+                                newStep to newCount
                             }
-                            val newStep = Step(newPos, newDir)
-                            newStep to newCount
-                        }
-                        .filter { it.first.pos !in config.obstacles }
-                        .filter { it.first !in visited }
+                            .filter { it.first.pos !in config.obstacles }
+                            .filter { it.first !in path.first }
+                            .map { (path.first + it.first) to it.second }
+                    }
                 }
             }
-            return -1
+            return res.size
         }
     }
 }

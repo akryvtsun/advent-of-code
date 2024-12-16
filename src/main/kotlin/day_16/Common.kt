206518a9db368e29705610e2c1aa3bb9b36ab9ab
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-16 08:09:38 +0200

    task 1 init

--- src/main/kotlin/day_16/Common.kt
+++ src/main/kotlin/day_16/Common.kt
@@ -1,17 +1,17 @@
 package day_16
 
 data class Point(val y: Int, val x: Int) {
-//    operator fun plus(other: Point): Point {
-//        return Point(y + other.y, x + other.x)
-//    }
+    operator fun plus(other: Point): Point {
+        return Point(y + other.y, x + other.x)
+    }
 }
 
-//enum class Command(val c: Char, val delta: Point) {
-//    UP('^', Point(-1, 0)),
-//    DOWN('v', Point(1, 0)),
-//    LEFT('<', Point(0, -1)),
-//    RIGHT('>', Point(0, 1));
-//}
+enum class Direction(val delta: Point) {
+    UP(Point(-1, 0)),
+    DOWN(Point(1, 0)),
+    LEFT(Point(0, -1)),
+    RIGHT(Point(0, 1));
+}
 
 data class Labyrinth(val start: Point, val end: Point, val obstacles: List<Point>)
 
--- src/main/kotlin/day_16/Task1.kt
+++ src/main/kotlin/day_16/Task1.kt
@@ -2,16 +2,36 @@ package day_16
 
 class Task1 {
 
-    data class Path(val steps: Long, val turns: Long) {
-        fun evaluate(): Long  = turns * 1000 + steps
-    }
+    data class Step(val pos: Point, val dir: Direction)
 
     companion object {
 
+        fun evaluate(path: List<Step>): Long {
+            var steps = 0L
+            var turns = 0L
+            path.zipWithNext()
+                .forEach { (a, b) -> if (a.dir == b.dir) steps++ else turns++ }
+            return turns * 1000 + steps
+        }
+
         fun solve(config: Labyrinth): Long {
-            val s: Sequence<Path> = sequenceOf(Path(0, 0))
-            return s
-                .minOf { it.evaluate() }
+            val pathList = mutableListOf<List<Step>>()
+
+            fun explore(path: List<Step>) {
+                val curPos = path.last()
+                if (curPos.pos == config.end) {
+                    pathList.add(path)
+                }
+                else {
+                    Direction.entries
+                        .map { Step(curPos.pos + it.delta, it) }
+                        .filter { it !in path }
+                        .forEach { explore(path + it) }
+                }
+            }
+            explore(listOf(Step(config.start, Direction.RIGHT)))
+
+            return pathList.minOf { evaluate(it) }
         }
     }
 }

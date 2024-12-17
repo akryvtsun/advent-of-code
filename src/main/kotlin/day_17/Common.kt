185d445c0329f55b0b438deab1c57b6d35c74102
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-17 08:18:49 +0200

    task 1 init impl

--- src/main/kotlin/day_17/Common.kt
+++ src/main/kotlin/day_17/Common.kt
@@ -1,6 +1,6 @@
 package day_17
 
-data class Computer(val A: Long, val B: Long, val C: Long, val program: List<Byte>)
+data class Computer(val A: Int, val B: Int, val C: Int, val program: List<Int>)
 
 fun transform(input: String): Computer {
     val blocks = input.split("\n\n")
@@ -8,11 +8,11 @@ fun transform(input: String): Computer {
     val A =  register(regs[0])
     val B =  register(regs[1])
     val C =  register(regs[2])
-    val program = blocks[1].substringAfterLast(" ").split(",").map { it.toByte() }
+    val program = blocks[1].substringAfterLast(" ").split(",").map { it.toInt() }
     return Computer(A, B, C, program)
 }
 
-fun register(data: String) = data.substringAfterLast(" ").toLong()
+fun register(data: String) = data.substringAfterLast(" ").toInt()
 
 
 
--- src/main/kotlin/day_17/Task1.kt
+++ src/main/kotlin/day_17/Task1.kt
@@ -5,7 +5,57 @@ class Task1 {
     companion object {
 
         fun solve(config: Computer): String {
-            return ""
+            var output = mutableListOf<Int>()
+
+            var A = config.A
+            var B = config.B
+            var C = config.C
+            var i = 0
+
+            fun getOperand(oper: Int): Int =
+                when (oper) {
+                    in 0..3 -> oper
+                    4 -> A
+                    5 -> B
+                    6 -> C
+                    else -> throw IllegalArgumentException("Invalid operation")
+                }
+
+            while (i < config.program.size) {
+                val opcode = config.program[i++]
+                val operand = getOperand(config.program[i++])
+                when (opcode) {
+                    0 -> { // adv
+                        A /= (2 shl operand)
+                    }
+                    1 -> { // bxl
+                        B = B xor operand
+                    }
+                    2 -> { // bst
+                        B = operand and 0b111
+                    }
+                    3 -> { // jnz
+                        if (A != 0) {
+                            i = operand
+                        }
+                    }
+                    4 -> { // bxc
+                        B = B xor C
+                    }
+                    5 -> { // out
+                        output += operand and 0b111
+                    }
+                    6 -> { // bdv
+                        B = A / (2 shl operand)
+                    }
+                    7 -> { // cdv
+                        C = A / (2 shl operand)
+                    }
+                    else -> throw IllegalArgumentException("Invalid operation")
+                }
+            }
+
+            return output.joinToString(separator = ",")
         }
     }
 }

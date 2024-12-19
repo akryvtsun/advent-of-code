cb10f7f61e04ea083b24364514561febcd34bbac
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-19 20:16:10 +0200

    task 2 init

---
+++ src/test/kotlin/day_19/Task2Test.kt
@@ -0,0 +1,33 @@
+package day_19
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Task2Test {
+
+    @Test
+    fun `should successfully pass task example`() {
+        val input = """
+            r, wr, b, g, bwu, rb, gb, br
+            
+            brwrr
+            bggr
+            gbbr
+            rrbgbr
+            ubwu
+            bwurrg
+            brgr
+            bbrgwb
+        """.trimIndent()
+        val (patterns, designs) = transform(input)
+        assertEquals(16, Task2.solve(patterns, designs))
+    }
+
+    @Test
+    fun `should successfully solve the real task`() {
+        val input = File("src/test/resources/day_19/TaskData.txt").readText()
+        val (patterns, designs) = transform(input)
+        assertEquals(-1, Task2.solve(patterns, designs))
+    }
+}

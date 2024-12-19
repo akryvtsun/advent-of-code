e35c7cb375b95f54983cc2c8cbbb8b845c90fc68
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-19 08:05:45 +0200

    task 19.1 impl

--- src/main/kotlin/day_19/Task1.kt
+++ src/main/kotlin/day_19/Task1.kt
@@ -5,7 +5,20 @@ class Task1 {
     companion object {
 
         fun solve(patterns: List<String>, designs: List<String>): Int {
-            return 0
+            return designs.count { isPossible(it, patterns).also { p -> println("$it=$p") } }
+        }
+
+        private fun isPossible(design: String, patterns: List<String>): Boolean {
+            val queue = ArrayDeque(listOf(design))
+            while (true) {
+                if (queue.isEmpty()) return false
+                val tail = queue.removeFirst()
+                val newTails = patterns
+                    .filter { tail.startsWith(it) }
+                    .map { tail.removePrefix(it) }
+                if (newTails.any { it.isEmpty() }) return true
+                queue.addAll(newTails)
+            }
         }
     }
 }

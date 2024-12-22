56a310d6082ad72c4d7a93847c8615032f4ce291
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-22 08:25:47 +0200

    task 1 init

---
+++ src/main/kotlin/day_22/Common.kt
@@ -0,0 +1,11 @@
+package day_22
+
+fun transform(input: String): List<Long> {
+    return input.lines().map(String::toLong)
+}
+
+
+
+
+
+
---
+++ src/main/kotlin/day_22/Task1.kt
@@ -0,0 +1,17 @@
+package day_22
+
+data class Node(var isTerminal: Boolean = false, var next: MutableMap<Char, Node>? = null)
+
+class Task1 {
+
+    companion object {
+
+        fun nextSecret(num: Long): Long {
+            return 0
+        }
+
+        fun solve(buyers: List<Long>): Long {
+            return 0
+        }
+    }
+}

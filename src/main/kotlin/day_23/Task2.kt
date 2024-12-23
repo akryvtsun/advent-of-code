bbf03e8929afc5ffd2c2b4bfed1bf866e0860954
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-23 18:12:50 +0200

    task 2 partial solution

--- src/main/kotlin/day_23/Task2.kt
+++ src/main/kotlin/day_23/Task2.kt
@@ -10,11 +10,37 @@ class Task2 {
         }
 
         fun solve(links: List<Pair<String, String>>): String {
+            // build model
             val model = mutableMapOf<String, MutableSet<String>>()
             for (link in links) {
                 addPair(model, link)
             }
-            return ""
+            // find max party
+            val maxParty = buildSet {
+                for (key in model.keys) {
+                    val party = mutableSetOf<String>()
+
+                    fun connectWithAllPartyElems(elem: String) = party.all { elem in model[it]!! }
+
+                    val queue = ArrayDeque<Pair<String, Set<String>>>()
+                    queue.add(key to model[key]!!)
+                    while (queue.isNotEmpty()) {
+                        val candidate = queue.removeFirst()
+                        for (host in candidate.second) {
+                            val n = model[host]!!
+                            val next = n.filter { it !in party && connectWithAllPartyElems(it) }
+                            if (next.isNotEmpty()) {
+                                party += candidate.first
+                                queue.add(host to next.toSet())
+                            }
+                        }
+                    }
+                    add(party)
+                }
+            }
+                .maxBy { it.size }
+            // create party password
+            return maxParty.sorted().joinToString(separator = ",")
         }
     }
 }

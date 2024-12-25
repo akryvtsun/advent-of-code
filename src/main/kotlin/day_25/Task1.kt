9b01b3068415311535b81bb1b9bffb4159130ac6
Andriy Kryvtsun <kontiky@gmail.com>
2024-12-25 07:34:39 +0200

    task 1 init

---
+++ src/main/kotlin/day_25/Task1.kt
@@ -0,0 +1,18 @@
+package day_25
+
+typealias Lock = List<Int>
+typealias Key = List<Int>
+
+class Task1 {
+
+    companion object {
+
+        fun transform(input: String): Pair<List<Lock>, List<Key>> {
+            return Pair(emptyList(), emptyList())
+        }
+
+        fun solve(locks: List<Lock>, keys: List<Key>): Long {
+            return 0
+        }
+    }
+}
---
+++ src/test/kotlin/day_25/Task1Test.kt
@@ -0,0 +1,62 @@
+package day_25
+
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class Task1Test {
+
+    @Test
+    fun `should successfully pass task example`() {
+        val input = """
+            #####
+            .####
+            .####
+            .####
+            .#.#.
+            .#...
+            .....
+            
+            #####
+            ##.##
+            .#.##
+            ...##
+            ...#.
+            ...#.
+            .....
+            
+            .....
+            #....
+            #....
+            #...#
+            #.#.#
+            #.###
+            #####
+            
+            .....
+            .....
+            #.#..
+            ###..
+            ###.#
+            ###.#
+            #####
+            
+            .....
+            .....
+            .....
+            #....
+            #.#..
+            #.#.#
+            #####
+        """.trimIndent()
+        val (locks, keys) = Task1.transform(input)
+        assertEquals(3, Task1.solve(locks, keys))
+    }
+
+    @Test
+    fun `should successfully solve the real task`() {
+        val input = File("src/test/resources/day_25/TaskData.txt").readText()
+        val (locks, keys) = Task1.transform(input)
+        assertEquals(-1, Task1.solve(locks, keys))
+    }
+}

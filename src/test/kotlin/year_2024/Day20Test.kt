702a7550d109a5d51e4d7f49eecb2d90b1600d3e
Andriy Kryvtsun <kontiky@gmail.com>
2025-01-05 16:06:23 +0200

    refactoring

---
+++ src/test/kotlin/year_2024/Day20Test.kt
@@ -0,0 +1,60 @@
+package year_2024
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import year_2024.day_20.Task1
+import year_2024.day_20.Task2
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 20: Race Condition")
+class Day20Test {
+
+    val input = """
+        ###############
+        #...#...#.....#
+        #.#.#.#.#.###.#
+        #S#...#.#.#...#
+        #######.#.#.###
+        #######.#.#...#
+        #######.#.###.#
+        ###..E#...#...#
+        ###.#######.###
+        #...###...#...#
+        #.#####.#.###.#
+        #.#...#.#.#...#
+        #.#.#.#.#.#.###
+        #...#...#...###
+        ###############
+    """.trimIndent()
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `Matches example`() {
+            assertEquals(5, Task1.solve(input, 20))
+        }
+
+        @Test
+        fun `Actual answer`() {
+            val input = readTaskData(2024, 20)
+            assertEquals(1358, Task1.solve(input, 100))
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `Matches example`() {
+            assertEquals(41, Task2.solve(input, 70))
+        }
+
+        @Test
+        fun `Actual answer`() {
+            val input = readTaskData(2024, 20)
+            assertEquals(1005856, Task2.solve(input, 100))
+        }
+    }
+}

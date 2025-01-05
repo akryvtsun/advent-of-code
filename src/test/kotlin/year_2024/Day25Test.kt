9238d20dc6326d98c27a18ef6f4f7f1c44a73d63
Andriy Kryvtsun <kontiky@gmail.com>
2025-01-05 16:00:14 +0200

    refactoring

---
+++ src/test/kotlin/year_2024/Day25Test.kt
@@ -0,0 +1,69 @@
+package year_2024
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import year_2024.day_25.Task1
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 25: Code Chronicle")
+class Day25Test {
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `Matches example`() {
+            val input = """
+                #####
+                .####
+                .####
+                .####
+                .#.#.
+                .#...
+                .....
+                
+                #####
+                ##.##
+                .#.##
+                ...##
+                ...#.
+                ...#.
+                .....
+                
+                .....
+                #....
+                #....
+                #...#
+                #.#.#
+                #.###
+                #####
+                
+                .....
+                .....
+                #.#..
+                ###..
+                ###.#
+                ###.#
+                #####
+                
+                .....
+                .....
+                .....
+                #....
+                #.#..
+                #.#.#
+                #####
+            """.trimIndent()
+            val (locks, keys) = Task1.transform(input)
+            assertEquals(3, Task1.solve(locks, keys))
+        }
+
+        @Test
+        fun `Actual answer`() {
+            val input = readTaskData(2024, 25)
+            val (locks, keys) = Task1.transform(input)
+            assertEquals(3077, Task1.solve(locks, keys))
+        }
+    }
+}

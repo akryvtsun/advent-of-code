3e4e7a8d9cb6dd32d9097d28d81f0dd246689c55
Andriy Kryvtsun <kontiky@gmail.com>
2025-01-11 17:16:24 +0200

    Year 2023 task 2 init

--- src/test/kotlin/year_2023/Day01Test.kt
+++ src/test/kotlin/year_2023/Day01Test.kt
@@ -4,22 +4,23 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import taskData
 import year_2023.day_01.Task1
+import year_2023.day_01.Task2
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @DisplayName("Day 1: Trebuchet?!")
 class Day01Test {
 
-    val input = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
-    """.trimIndent()
-
     @Nested
     @DisplayName("Part 1")
     inner class Part1 {
+        val input = """
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+        """.trimIndent()
+
         @Test
         fun `Matches example`() {
             assertEquals(142, Task1.solve(input))
@@ -28,7 +29,32 @@ class Day01Test {
         @Test
         fun `Actual answer`() {
             val input = taskData(2023, 1).readText()
-            assertEquals(-1, Task1.solve(input))
+            assertEquals(54338, Task1.solve(input))
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        val input = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()
+
+        @Test
+        fun `Matches example`() {
+            assertEquals(281, Task2.solve(input))
+        }
+
+        @Test
+        fun `Actual answer`() {
+            val input = taskData(2023, 1).readText()
+            assertEquals(-1, Task2.solve(input))
         }
     }
 }

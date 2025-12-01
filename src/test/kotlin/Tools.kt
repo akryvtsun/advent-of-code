3549f2e0aa94ebfcfb503f115f3996440b8980cc
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-01 14:08:25 +0200

    refactoring

--- src/test/kotlin/Tools.kt
+++ src/test/kotlin/Tools.kt
@@ -1,5 +1,7 @@
 import java.io.File
 
+val UNKNOWN_VALUE = -1
+
 @Deprecated("Use class TaskData")
 fun taskData(year: Int, task: Int) =
     File("src/test/resources/year_$year/day_${String.format("%02d", task)}_data.txt")

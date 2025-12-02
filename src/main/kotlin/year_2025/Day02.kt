b6e93138c3bfb54d6c9a8c9944ad7bed78a7dbff
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-02 08:17:11 +0200

    day 2 with tests solving

--- src/main/kotlin/year_2025/Day02.kt
+++ src/main/kotlin/year_2025/Day02.kt
@@ -3,25 +3,22 @@ package year_2025
 class Day02(private val input: String) {
 
     fun solvePart1(): Long {
+        fun isInvalid(n: Long): Boolean {
+            val s = n.toString()
+            if (s.length % 2 == 0) {
+                val popolam = s.length / 2
+                val first = s.take(popolam)
+                val second = s.drop(popolam)
+                return first == second
+            }
+            return false
+        }
+
         return input.split(",")
-            .flatMap {
-                val (from, to) = it.split("-")
-                //println("from: $from, to: $to")
-                val res = mutableListOf<Long>()
-                for (i in (from.toLong())..(to.toLong())) {
-                    val s = i.toString()
-                    if (s.length % 2 == 0) {
-                        val popolam = s.length / 2
-                        //println(popolam)
-                        val first = s.substring(0, popolam).toLong()
-                        val second = s.drop(popolam).toLong()
-                        if (first == second) {
-                            res.add(i)
-                        }
-                    }
-                }
-                res
-            }.sum()
+            .sumOf {
+                val (from, to) = it.split("-").map(String::toLong)
+                (from..to).sumOf { if (isInvalid(it)) it else 0 }
+            }
     }
 
     fun solvePart2(): Long {
@@ -30,25 +27,17 @@ class Day02(private val input: String) {
             for (i in 2..(s.length)) {
                 if (s.length % i == 0) {
                     val len = s.length / i
-                    val set = mutableSetOf<String>()
-                    set.addAll(s.chunked(len))
-                    if (set.size == 1) return true
+                    if (s.chunked(len).distinct().size == 1) return true
                 }
             }
             return false
         }
 
         return input.split(",")
-            .flatMap {
-                val (from, to) = it.split("-")
-                val res = mutableListOf<Long>()
-                for (i in (from.toLong())..(to.toLong())) {
-                    if (isInvalid(i)) {
-                        res.add(i)
-                    }
-                }
-                res
-            }.sum()
+            .sumOf {
+                val (from, to) = it.split("-").map(String::toLong)
+                (from..to).sumOf { if (isInvalid(it)) it else 0 }
+            }
     }
 }
 

ad0f5fc95cd0b179d6c014d2bcd18869a2955edd
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-02 07:58:18 +0200

    day 2 with tests solving

--- src/main/kotlin/year_2025/Day02.kt
+++ src/main/kotlin/year_2025/Day02.kt
@@ -1,15 +1,54 @@
 package year_2025
 
-import UNKNOWN_VALUE
+class Day02(private val input: String) {
 
-class Day02(private val input: List<String>) {
-
-    fun solvePart1(): Int {
-        return UNKNOWN_VALUE
+    fun solvePart1(): Long {
+        return input.split(",")
+            .flatMap {
+                val (from, to) = it.split("-")
+                //println("from: $from, to: $to")
+                val res = mutableListOf<Long>()
+                for (i in (from.toLong())..(to.toLong())) {
+                    val s = i.toString()
+                    if (s.length % 2 == 0) {
+                        val popolam = s.length / 2
+                        //println(popolam)
+                        val first = s.substring(0, popolam).toLong()
+                        val second = s.drop(popolam).toLong()
+                        if (first == second) {
+                            res.add(i)
+                        }
+                    }
+                }
+                res
+            }.sum()
     }
 
-    fun solvePart2(): Int {
-        return UNKNOWN_VALUE
+    fun solvePart2(): Long {
+        fun isInvalid(n: Long): Boolean {
+            val s = n.toString()
+            for (i in 2..(s.length)) {
+                if (s.length % i == 0) {
+                    val len = s.length / i
+                    val set = mutableSetOf<String>()
+                    set.addAll(s.chunked(len))
+                    if (set.size == 1) return true
+                }
+            }
+            return false
+        }
+
+        return input.split(",")
+            .flatMap {
+                val (from, to) = it.split("-")
+                val res = mutableListOf<Long>()
+                for (i in (from.toLong())..(to.toLong())) {
+                    if (isInvalid(i)) {
+                        res.add(i)
+                    }
+                }
+                res
+            }.sum()
     }
 }
 
--- src/test/kotlin/year_2025/Day02Test.kt
+++ src/test/kotlin/year_2025/Day02Test.kt
@@ -1,37 +1,36 @@
 package year_2025
 
 import TaskData
-import UNKNOWN_VALUE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-@DisplayName("")
+@DisplayName("Day 2: Gift Shop")
 class Day02Test {
     companion object {
-        val testInput = """
-        """.trimIndent().lines()
+        val testInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528," +
+                "446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
-        val realInput = TaskData(2025, 2).asLines()
+        val realInput = TaskData(2025, 2).asString()
 
         @JvmStatic
         fun part1Data() = listOf(
-            arguments(testInput, UNKNOWN_VALUE),
-            arguments(realInput, UNKNOWN_VALUE)
+            arguments(testInput, 1227775554),
+            arguments(realInput, 15873079081)
         )
 
         @JvmStatic
         fun part2Data() = listOf(
-            arguments(testInput, UNKNOWN_VALUE),
-            arguments(realInput, UNKNOWN_VALUE)
+            arguments(testInput, 4174379265),
+            arguments(realInput, 22617871034)
         )
     }
 
     @ParameterizedTest
     @MethodSource("part1Data")
-    fun part1Test(input: List<String>, result: Int) {
+    fun part1Test(input: String, result: Long) {
         assertThat(
             Day02(input).solvePart1()
         ).isEqualTo(result)
@@ -39,7 +38,7 @@ class Day02Test {
 
     @ParameterizedTest
     @MethodSource("part2Data")
-    fun part2Test(input: List<String>, result: Int) {
+    fun part2Test(input: String, result: Long) {
         assertThat(
             Day02(input).solvePart2()
         ).isEqualTo(result)

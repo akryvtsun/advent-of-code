ab4a6ee22afc4b45f29c3af85093b4bbc55904b3
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-04 08:23:43 +0200

    day 4, part 2 complete

--- src/main/kotlin/year_2025/Day04.kt
+++ src/main/kotlin/year_2025/Day04.kt
@@ -1,27 +1,29 @@
 package year_2025
 
+typealias Surface = List<String>
+
 class Day04(input: String) {
 
-    private val map = input.lines()
+    private val map: Surface = input.lines()
 
     data class Point(val y: Int, val x: Int)
 
-    private val rolls = sequence {
+    fun Surface.rolls() = sequence {
         for (y in map.indices) {
-            for (x in map[y].indices) {
-                if (map[y][x] == '@') {
+            for (x in this@rolls[y].indices) {
+                if (this@rolls[y][x] == '@') {
                     yield(Point(y, x))
                 }
             }
         }
     }
 
-    private fun isAccessible(candidate: Point): Boolean {
+    fun Surface.isAccessible(candidate: Point): Boolean {
         val places = sequence {
             for (y in candidate.y - 1..candidate.y + 1) {
                 for (x in candidate.x - 1..candidate.x + 1) {
-                    if (y in map.indices && x in map[y].indices) {
-                        yield(map[y][x])
+                    if (y in this@isAccessible.indices && x in this@isAccessible[y].indices) {
+                        yield(this@isAccessible[y][x])
                     }
                 }
             }
@@ -30,11 +32,29 @@ class Day04(input: String) {
     }
 
     fun solvePart1(): Int {
-        return rolls.count { isAccessible(it) }
+        return map.rolls().count { map.isAccessible(it) }
     }
 
     fun solvePart2(): Int {
-        return rolls.count { isAccessible(it) }
+        var count = 0
+        var temp = map
+        while (true) {
+            val temp2 = temp.toMutableList()
+            val acc = temp.rolls().count {
+                val isAcc = temp.isAccessible(it)
+                if (isAcc) {
+                    val sb = StringBuilder(temp2[it.y])
+                    sb.setCharAt(it.x, '.')
+                    temp2[it.y] = sb.toString()
+                }
+                isAcc
+            }
+            println(acc)
+            if (acc == 0) break
+            count += acc
+            temp = temp2
+        }
+        return count
     }
 }
 

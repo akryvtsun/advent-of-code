3b27284ea651301b31c2b68858c373bf88fe557e
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-04 07:24:39 +0200

    inintial day 4 impl

---
+++ src/main/kotlin/year_2025/Day04.kt
@@ -0,0 +1,13 @@
+package year_2025
+
+import UNKNOWN_VALUE
+
+class Day04(input: String) {
+
+    private val banks = input.lines()
+
+    fun solvePart1() = UNKNOWN_VALUE
+
+    fun solvePart2() = UNKNOWN_VALUE
+}
+

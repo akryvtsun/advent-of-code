32f18eda88397a38911a5df0fb95956a37dfa0bf
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-04 08:41:43 +0200

    refactoring

--- src/main/kotlin/year_2025/Day04.kt
+++ src/main/kotlin/year_2025/Day04.kt
@@ -8,51 +8,42 @@ class Day04(input: String) {
 
     data class Point(val y: Int, val x: Int)
 
-    fun Surface.rolls() = sequence {
+    fun Surface.rolls() = buildList {
         for (y in map.indices) {
             for (x in this@rolls[y].indices) {
                 if (this@rolls[y][x] == '@') {
-                    yield(Point(y, x))
+                    add(Point(y, x))
                 }
             }
         }
     }
 
-    fun Surface.isAccessible(candidate: Point): Boolean {
+    fun Surface.isAccessible(candidate: Point, r: List<Point>): Boolean {
         val places = sequence {
             for (y in candidate.y - 1..candidate.y + 1) {
                 for (x in candidate.x - 1..candidate.x + 1) {
                     if (y in this@isAccessible.indices && x in this@isAccessible[y].indices) {
-                        yield(this@isAccessible[y][x])
+                        yield(Point(y, x))
                     }
                 }
             }
         }
-        return places.count { it == '@' } - 1 < 4
+        return places.count { it in r } - 1 < 4
     }
 
     fun solvePart1(): Int {
-        return map.rolls().count { map.isAccessible(it) }
+        val r = map.rolls()
+        return r.count { map.isAccessible(it, r) }
     }
 
     fun solvePart2(): Int {
         var count = 0
-        var temp = map
+        var temp = map.rolls()
         while (true) {
-            val temp2 = temp.toMutableList()
-            val acc = temp.rolls().count {
-                val isAcc = temp.isAccessible(it)
-                if (isAcc) {
-                    val sb = StringBuilder(temp2[it.y])
-                    sb.setCharAt(it.x, '.')
-                    temp2[it.y] = sb.toString()
-                }
-                isAcc
-            }
-            println(acc)
-            if (acc == 0) break
-            count += acc
-            temp = temp2
+            val isAcc = temp.filter { map.isAccessible(it, temp) }
+            if (isAcc.isEmpty()) break
+            count += isAcc.size
+            temp = temp - isAcc
         }
         return count
     }

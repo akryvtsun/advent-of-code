e25f84c0f634286444a33dd1bcb3d44fbd41d94f
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-04 08:29:51 +0200

    day 4, part 2 complete

--- src/test/kotlin/year_2025/Day04Test.kt
+++ src/test/kotlin/year_2025/Day04Test.kt
@@ -1,7 +1,6 @@
 package year_2025
 
 import TaskData
-import UNKNOWN_VALUE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
@@ -35,7 +34,7 @@ class Day04Test {
         @JvmStatic
         fun part2Data() = listOf(
             arguments(testInput, 43),
-            arguments(realInput, UNKNOWN_VALUE)
+            arguments(realInput, 8936)
         )
     }
 

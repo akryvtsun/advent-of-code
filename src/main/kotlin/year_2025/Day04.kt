fcb263abadf7cb9610844b85825749ef9f2976ce
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-04 08:52:50 +0200

    refactoring

--- src/main/kotlin/year_2025/Day04.kt
+++ src/main/kotlin/year_2025/Day04.kt
@@ -8,7 +8,7 @@ class Day04(input: String) {
 
     data class Point(val y: Int, val x: Int)
 
-    fun Surface.rolls() = buildList {
+    fun Surface.rolls() = buildSet {
         for (y in map.indices) {
             for (x in this@rolls[y].indices) {
                 if (this@rolls[y][x] == '@') {
@@ -18,32 +18,32 @@ class Day04(input: String) {
         }
     }
 
-    fun Surface.isAccessible(candidate: Point, r: List<Point>): Boolean {
+    fun Set<Point>.isAccessible(candidate: Point): Boolean {
         val places = sequence {
             for (y in candidate.y - 1..candidate.y + 1) {
                 for (x in candidate.x - 1..candidate.x + 1) {
-                    if (y in this@isAccessible.indices && x in this@isAccessible[y].indices) {
+                    if (y in map.indices && x in map[y].indices) {
                         yield(Point(y, x))
                     }
                 }
             }
         }
-        return places.count { it in r } - 1 < 4
+        return places.count { it in this } - 1 < 4
     }
 
     fun solvePart1(): Int {
         val r = map.rolls()
-        return r.count { map.isAccessible(it, r) }
+        return r.count { r.isAccessible(it) }
     }
 
     fun solvePart2(): Int {
         var count = 0
-        var temp = map.rolls()
+        val temp = map.rolls().toMutableSet()
         while (true) {
-            val isAcc = temp.filter { map.isAccessible(it, temp) }
+            val isAcc = temp.filter { temp.isAccessible(it) }.toSet()
             if (isAcc.isEmpty()) break
             count += isAcc.size
-            temp = temp - isAcc
+            temp -= isAcc
         }
         return count
     }

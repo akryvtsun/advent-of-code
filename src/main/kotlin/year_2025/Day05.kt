97e096a7054f716106f056358f7f7ec9fdfceaff
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-05 08:46:21 +0200

    year 2025, day 5, part 2 init impl 2

--- src/main/kotlin/year_2025/Day05.kt
+++ src/main/kotlin/year_2025/Day05.kt
@@ -2,14 +2,12 @@ package year_2025
 
 class Day05(input: String) {
 
-    val idRanges = input.substringBefore("\n\n").lines()
-        .map {
+    val idRanges = input.substringBefore("\n\n").lines().map {
             val range = it.trim().split("-")
             range[0].toLong()..range[1].toLong()
         }
 
-    val ids = input.substringAfter("\n\n").lines()
-        .map(String::toLong)
+    val ids = input.substringAfter("\n\n").lines().map(String::toLong)
 
     fun solvePart1(): Int {
         return ids.count { id ->
@@ -17,14 +15,35 @@ class Day05(input: String) {
         }
     }
 
-    fun solvePart2(): Int {
-        return idRanges.fold(mutableSetOf<Long>())
-        { acc, e ->
-            acc.addAll(e.iterator().asSequence().toSet())
-            acc
-        }.size
-//
-//        return idRanges
-//            .sumOf { it.last - it.first + 1 }
+    fun solvePart2(): Long {
+        tailrec fun merge(ranges: Set<LongRange>) : Set<LongRange> {
+            var changed = false
+            val newRanges = ranges.fold(mutableSetOf<LongRange>()) { acc, e ->
+                if (acc.isEmpty()) {
+                    acc.add(e)
+                    acc
+                } else {
+                    val newAcc = mutableSetOf<LongRange>()
+                    var wasMerged = false
+                    for (ae in acc) {
+                        if (ae.last <= e.first || e.last <= ae.first) {
+                            newAcc.add(ae)
+                            continue
+                        }
+                        newAcc.add(minOf(ae.first, e.first)..maxOf(ae.last, e.last))
+                        wasMerged = true
+                        changed = true
+                    }
+                    if (!wasMerged) newAcc.add(e)
+                    newAcc
+                }
+            }
+            return if (changed) merge(newRanges) else newRanges
+        }
+
+        println(idRanges)
+        return merge(idRanges.toSet())
+            .also { println(it) }
+            .sumOf { it.last - it.first + 1 }
     }
 }

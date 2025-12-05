8031341dcfca829998d1ddea7ce0211767cfc9ab
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-05 21:53:47 +0200

    year 2024, day 19, refactoring

---
+++ src/main/kotlin/year_2024/Day19.kt
@@ -0,0 +1,92 @@
+package year_2024
+
+import java.util.PriorityQueue
+
+class Day19(private val input: String) {
+
+    fun transform(input: String): Pair<List<String>, List<String>> {
+        val (patterns, designs) = input.split("\n\n")
+        return patterns.split(", ") to designs.lines()
+    }
+
+    data class Node(var isTerminal: Boolean = false, var next: MutableMap<Char, Node>? = null)
+
+    fun buildPrefixTree(patterns: List<String>): Node {
+        val root = Node()
+        for (pattern in patterns) {
+            var candidate = root
+            for (c in pattern) {
+                if (candidate.next == null) {
+                    candidate.next = mutableMapOf()
+                }
+                candidate = candidate.next!!.getOrPut(c) { Node() }
+            }
+            candidate.isTerminal = true
+        }
+        return root
+    }
+
+    fun findTails(design: String, root: Node, tail: Int) : List<Int> {
+        val newTails = mutableListOf<Int>()
+        var i = tail
+        var node = root
+
+        while (true) {
+            if (node.next == null || i == design.length) break
+            val c = design[i]
+            val candidate = node.next!![c]
+            if (candidate != null && candidate.isTerminal) {
+                newTails += i + 1
+            }
+            if (candidate == null) break
+            node = candidate
+            i++
+        }
+
+        return newTails
+    }
+
+    fun solvePart1(): Int {
+        fun isPossible(design: String, root: Node): Boolean {
+            val queue = LinkedHashSet(listOf(0))
+            while (true) {
+                if (queue.isEmpty()) return false
+                val tail = queue.removeFirst()
+                if (tail == design.length) return true
+                val newTails = findTails(design, root, tail)
+                queue.addAll(newTails)
+            }
+        }
+
+        val (patterns, designs) = transform(input)
+        val root = buildPrefixTree(patterns)
+        return designs.count { isPossible(it, root) }
+    }
+
+    fun solvePart2(): Long {
+        fun variants(design: String, root: Node): Long {
+            val queue = PriorityQueue<Pair<Int, Long>>(compareBy { it.first })
+            queue.add(0 to 1L)
+            while (true) {
+                if (queue.isEmpty()) return 0
+                val (tIndex, tCount) = queue.remove()
+                if (tIndex == design.length) return tCount
+                val newTails = findTails(design, root, tIndex)
+                newTails
+                    .forEach { t ->
+                        var pair = queue.firstOrNull { it.first == t }
+                        if (pair != null) {
+                            queue.remove(pair)
+                        } else {
+                            pair = t to 0L
+                        }
+                        queue.add(t to pair.second + tCount)
+                    }
+            }
+        }
+
+        val (patterns, designs) = transform(input)
+        val root = buildPrefixTree(patterns)
+        return designs.sumOf { variants(it, root) }
+    }
+}
--- src/test/kotlin/year_2024/Day19Test.kt
+++ src/test/kotlin/year_2024/Day19Test.kt
@@ -1,65 +1,56 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import taskData
-import year_2024.day_19.Task1
-import year_2024.day_19.Task2
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("Day 19: Linen Layout")
 class Day19Test {
-
-    val input = """
-        r, wr, b, g, bwu, rb, gb, br
+    companion object {
+        val testInput = """
+            r, wr, b, g, bwu, rb, gb, br
         
-        brwrr
-        bggr
-        gbbr
-        rrbgbr
-        ubwu
-        bwurrg
-        brgr
-        bbrgwb
-    """.trimIndent()
+            brwrr
+            bggr
+            gbbr
+            rrbgbr
+            ubwu
+            bwurrg
+            brgr
+            bbrgwb
+        """.trimIndent()
 
-    fun transform(input: String): Pair<List<String>, List<String>> {
-        val (patterns, designs) = input.split("\n\n")
-        return patterns.split(", ") to designs.lines()
+        val realInput = TaskData(2024, 19).asString()
+
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, 6),
+            arguments(realInput, 338)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput, 16),
+            arguments(realInput, 841533074412361)
+        )
     }
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `Matches example`() {
-            val (patterns, designs) = transform(input)
-            assertEquals(6, Task1.solve(patterns, designs))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 19).readText()
-            val (patterns, designs) = transform(input)
-            assertEquals(338, Task1.solve(patterns, designs))
-        }
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Int) {
+        assertThat(
+            Day19(input).solvePart1()
+        ).isEqualTo(result)
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Matches example`() {
-            val (patterns, designs) = transform(input)
-            assertEquals(16, Task2.solve(patterns, designs))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 19).readText()
-            val (patterns, designs) = transform(input)
-            assertEquals(841533074412361, Task2.solve(patterns, designs))
-        }
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day19(input).solvePart2()
+        ).isEqualTo(result)
     }
 }

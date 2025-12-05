06e1a50447c33e74194cc18ada60767b036c632f
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-05 06:53:12 +0200

    day 5 creation

---
+++ src/test/kotlin/year_2025/Day05Test.kt
@@ -0,0 +1,47 @@
+package year_2025
+
+import TaskData
+import UNKNOWN_VALUE
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@DisplayName("")
+class Day05Test {
+    companion object {
+        val testInput = """
+        """.trimIndent()
+
+        val realInput = TaskData(2025, 5).asString()
+
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, UNKNOWN_VALUE),
+            arguments(realInput, UNKNOWN_VALUE)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput, UNKNOWN_VALUE),
+            arguments(realInput, UNKNOWN_VALUE)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Int) {
+        assertThat(
+            Day05(input).solvePart1()
+        ).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day05(input).solvePart2()
+        ).isEqualTo(result)
+    }
+}

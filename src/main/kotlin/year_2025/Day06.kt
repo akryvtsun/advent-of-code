a95bde6001a4ffe1b75d787cdcf599e0f406f46f
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-06 08:57:38 +0200

    refactoring

--- src/main/kotlin/year_2025/Day06.kt
+++ src/main/kotlin/year_2025/Day06.kt
@@ -9,24 +9,27 @@ class Day06(val input: String) {
         return if (task.op == '+') realData.sum() else realData.reduce { a, b -> a * b }
     }
 
-    fun solve(preprocess: (List<String>) -> List<String>) : Long {
+    fun solve(preprOp: (List<String>) -> List<String>) : Long {
         val data = input.lines()
         val delims = data.last().withIndex().filter { (_, v) -> v != ' ' }
         val operands = data.dropLast(1)
         val tasks: List<Task> = delims.zipWithNext()
             .map { (d1, d2) -> Task(d1.value, operands.map { it.substring(d1.index, d2.index - 1) }) } +
                 Task(delims.last().value, operands.map { it.substring(delims.last().index) })
-        val processed = tasks.map { Task(it.op, preprocess(it.data)) }
+        val processed = tasks.map { Task(it.op, preprOp(it.data)) }
         return processed.sumOf { solveTask(it) }
     }
 
-    fun solvePart1(): Long = solve { it }
+    fun noOp(data: List<String>): List<String> = data
 
-    fun transpose(data: List<String>): List<String> {
-        return data.fold(List(data.first().length) { "" }) { acc, value ->
+    fun solvePart1(): Long = solve(::noOp)
+
+    fun transposeOp(data: List<String>): List<String> {
+        val length = data.maxOf { it.length }
+        return data.fold(List(length) { "" }) { acc, value ->
             value.mapIndexed { index, c -> acc[index] + c }
         }
     }
 
-    fun solvePart2(): Long = solve(::transpose)
+    fun solvePart2(): Long = solve(::transposeOp)
 }

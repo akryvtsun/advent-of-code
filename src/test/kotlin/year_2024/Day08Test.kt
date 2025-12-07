7f003c16058f1b07b214b56c9580d82471cda2b4
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-07 12:25:34 +0200

    refactoring

--- src/test/kotlin/year_2024/Day08Test.kt
+++ src/test/kotlin/year_2024/Day08Test.kt
@@ -1,81 +1,58 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import taskData
-import year_2024.day_08.Antenna
-import year_2024.day_08.Data
-import year_2024.day_08.Position
-import year_2024.day_08.Task1
-import year_2024.day_08.Task2
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("Day 8: Resonant Collinearity")
 class Day08Test {
+    companion object {
+        val testInput = """
+            ............
+            ........0...
+            .....0......
+            .......0....
+            ....0.......
+            ......A.....
+            ............
+            ............
+            ........A...
+            .........A..
+            ............
+            ............
+        """.trimIndent()
 
-    val input = """
-        ............
-        ........0...
-        .....0......
-        .......0....
-        ....0.......
-        ......A.....
-        ............
-        ............
-        ........A...
-        .........A..
-        ............
-        ............
-    """.trimIndent()
+        val realInput = TaskData(2024, 8).asString()
 
-    fun transform(input: String): Data {
-        val lines = input.split("\n")
-        val height = lines.size
-        val width = lines.first().length
-        val antennas = lines.flatMapIndexed { i, line ->
-            val lineList = mutableListOf<Antenna>()
-            for (j in line.indices) {
-                if (line[j] != '.') {
-                    lineList += Antenna(Position(i, j), line[j])
-                }
-            }
-            lineList
-        }
-        return Data(height, width, antennas)
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, 14),
+            arguments(realInput, 376)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput, 34),
+            arguments(realInput, 1352)
+        )
     }
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `Matches example`() {
-            val data = transform(input)
-            assertEquals(14, Task1.solve(data))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 8).readText()
-            val data = transform(input)
-            assertEquals(376, Task1.solve(data))
-        }
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Long) {
+        assertThat(
+            Day08(input).solvePart1()
+        ).isEqualTo(result)
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Matches example`() {
-            val data = transform(input)
-            assertEquals(34, Task2.solve(data))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 8).readText()
-            val data = transform(input)
-            assertEquals(1352, Task2.solve(data))
-        }
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day08(input).solvePart2()
+        ).isEqualTo(result)
     }
 }

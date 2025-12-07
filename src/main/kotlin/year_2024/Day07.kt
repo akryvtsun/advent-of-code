77c05a35a1f049e8b01fb7ed1267fb6d94ea3630
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-07 11:44:03 +0200

    year 2024, day 7 refactoring

---
+++ src/main/kotlin/year_2024/Day07.kt
@@ -0,0 +1,114 @@
+package year_2024
+
+class Day07(private var input: String) {
+
+    data class Equation(val test: Long, val nums: List<Long>)
+
+    fun transform(input: String): List<Equation> {
+        return input.split("\n")
+            .map { line ->
+                val test = line.substringBefore(':').toLong()
+                val nums = line.substringAfter(": ").split(" ").map { it.toLong() }
+                Equation(test, nums)
+            }
+    }
+
+    val data = transform(input)
+
+    enum class Operation{
+        PLUS {
+            override fun apply(arg1: Long, arg2: Long) = arg1 + arg2
+        },
+        MULTIPLY {
+            override fun apply(arg1: Long, arg2: Long) = arg1 * arg2
+        };
+
+        abstract fun apply(arg1: Long, arg2: Long): Long
+    }
+
+    fun solvePart1(): Long {
+        return data
+            .filter { isTrue(it) }
+            .sumOf { it.test }
+    }
+
+    private fun isTrue(equation: Equation): Boolean {
+        val opsPermutations = generateOps(equation.nums.size-1)
+        return opsPermutations.any { operations ->
+            equation.nums.reduceIndexed {
+                    index, acc, value -> operations[index-1].apply(acc, value)
+            } == equation.test
+        }
+    }
+
+//  Not optimal generation on Lists
+//        private fun generateOps(side: Int) : List<List<Operation>> {
+//            val permutations = mutableListOf<List<Operation>>()
+//            for (operation in Operation.entries) {
+//                if (side == 1) {
+//                    permutations.add(listOf(operation))
+//                }
+//                else {
+//                    for (permutation in generateOps(side-1)) {
+//                        permutations.add(listOf(operation) + permutation)
+//                    }
+//                }
+//            }
+//            return permutations
+//        }
+
+    private fun generateOps(side: Int) : Sequence<List<Operation>> = sequence {
+        for (operation in Operation.entries) {
+            if (side == 1) {
+                yield(listOf(operation))
+            }
+            else {
+                for (permutation in generateOps(side-1)) {
+                    yield(listOf(operation) + permutation)
+                }
+            }
+        }
+    }
+
+    enum class Operation2{
+        PLUS {
+            override fun apply(arg1: Long, arg2: Long) = arg1 + arg2
+        },
+        MULTIPLY {
+            override fun apply(arg1: Long, arg2: Long) = arg1 * arg2
+        },
+        CONCAT {
+            override fun apply(arg1: Long, arg2: Long) = "$arg1$arg2".toLong()
+        };
+
+        abstract fun apply(arg1: Long, arg2: Long): Long
+    }
+
+    fun solvePart2(): Long {
+        return data
+            .filter { isTrue2(it) }
+            .sumOf { it.test }
+    }
+
+    private fun isTrue2(equation: Equation): Boolean {
+        val opsPermutations = generateOps2(equation.nums.size-1)
+        return opsPermutations.any { operations ->
+            equation.nums.reduceIndexed {
+                    index, acc, value -> operations[index-1].apply(acc, value)
+            } == equation.test
+        }
+    }
+
+    private fun generateOps2(side: Int) : Sequence<List<Operation2>> = sequence {
+        for (operation in Operation2.entries) {
+            if (side == 1) {
+                yield(listOf(operation))
+            }
+            else {
+                for (permutation in generateOps2(side-1)) {
+                    yield(listOf(operation) + permutation)
+                }
+            }
+        }
+    }
+}
--- src/test/kotlin/year_2024/Day07Test.kt
+++ src/test/kotlin/year_2024/Day07Test.kt
@@ -1,69 +1,55 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import taskData
-import year_2024.day_07.Equation
-import year_2024.day_07.Task1
-import year_2024.day_07.Task2
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("Day 7: Bridge Repair")
 class Day07Test {
+    companion object {
+        val testInput = """
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
+        """.trimIndent()
 
-    val input = """
-        190: 10 19
-        3267: 81 40 27
-        83: 17 5
-        156: 15 6
-        7290: 6 8 6 15
-        161011: 16 10 13
-        192: 17 8 14
-        21037: 9 7 18 13
-        292: 11 6 16 20
-    """.trimIndent()
+        val realInput = TaskData(2024, 7).asString()
 
-    fun transform(input: String): List<Equation> {
-        return input.split("\n")
-            .map { line ->
-                val test = line.substringBefore(':').toLong()
-                val nums = line.substringAfter(": ").split(" ").map { it.toLong() }
-                Equation(test, nums)
-            }
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, 3749),
+            arguments(realInput, 12940396350192)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput, 11387),
+            arguments(realInput, 106016735664498)
+        )
     }
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `Matches example`() {
-            val data = transform(input)
-            assertEquals(3749, Task1.solve(data))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 7).readText()
-            val data = transform(input)
-            assertEquals(12940396350192, Task1.solve(data))
-        }
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Long) {
+        assertThat(
+            Day07(input).solvePart1()
+        ).isEqualTo(result)
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Matches example`() {
-            val data = transform(input)
-            assertEquals(11387, Task2.solve(data))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 7).readText()
-            val data = transform(input)
-            assertEquals(106016735664498, Task2.solve(data))
-        }
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day07(input).solvePart2()
+        ).isEqualTo(result)
     }
 }

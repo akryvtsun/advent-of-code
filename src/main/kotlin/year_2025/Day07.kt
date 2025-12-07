49ba120f6af29027cfa0da681f1434de9f07a08d
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-07 07:36:48 +0200

    year 2025, day 7, part 1 complete

--- src/main/kotlin/year_2025/Day07.kt
+++ src/main/kotlin/year_2025/Day07.kt
@@ -4,8 +4,36 @@ import UNKNOWN_VALUE
 
 class Day07(val input: String) {
 
+    data class Point(val y: Int, val x: Int)
+
     fun solvePart1(): Int {
-        return UNKNOWN_VALUE
+        val map = input.lines()
+
+        val startPos = Point(0, map.first().indexOf('S'))
+        val splitters: Set<Point> = map
+            .withIndex()
+            .flatMap { line ->
+                line.value.mapIndexedNotNull { x, c -> if (c == '^') Point(line.index, x) else null }
+            }.toSet()
+
+        fun inInMap(p: Point) = (p.y in 0..map.size) && (p.x in 0 .. map.first().length)
+
+        var count = 0
+        var beams = setOf(startPos)
+        while (true) {
+            val newPos: Set<Point> = beams.flatMap { b ->
+                val newB = Point(b.y + 1, b.x)
+                if (newB in splitters) {
+                    count++
+                    listOf(Point(newB.y, newB.x - 1), Point(newB.y, newB.x + 1))
+                }
+                else
+                    listOf(newB)
+            }.filter(::inInMap).toSet()
+            if (newPos.isEmpty()) break
+            beams = newPos
+        }
+        return count
     }
 
     fun solvePart2(): Int {

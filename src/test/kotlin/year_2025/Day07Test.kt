825efccfb4610b8b9cdf86d009a13ad009ad42ad
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-07 06:54:55 +0200

    year 2024, day 7 init

---
+++ src/test/kotlin/year_2025/Day07Test.kt
@@ -0,0 +1,47 @@
+package year_2025
+
+import TaskData
+import UNKNOWN_VALUE
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@DisplayName("Day 7")
+class Day07Test {
+    companion object {
+        val testInput = """
+        """.trimIndent()
+
+        val realInput = TaskData(2025, 7).asString()
+
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, UNKNOWN_VALUE),
+            arguments(realInput, UNKNOWN_VALUE)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput, UNKNOWN_VALUE),
+            arguments(realInput, UNKNOWN_VALUE)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Long) {
+        assertThat(
+            Day07(input).solvePart1()
+        ).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day07(input).solvePart2()
+        ).isEqualTo(result)
+    }
+}

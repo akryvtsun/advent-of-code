4b371254681728c8ad81ce9b11ed5d180b2efcfd
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-08 07:21:02 +0200

    year 2025, day 8, part 1 init

--- src/main/kotlin/year_2025/Day08.kt
+++ src/main/kotlin/year_2025/Day08.kt
@@ -1,12 +1,24 @@
 package year_2025
 
-class Day08(val input: String) {
+import UNKNOWN_VALUE
+
+class Day08(input: String) {
+
+    data class Point3d(val x: Int, val y: Int, val z: Int)
+
+    val boxes = input.lines()
+        .map {
+            val (x, y, z) = it.split(",").map(String::toInt)
+            Point3d(x, y, z)
+        }
 
     fun solvePart1(): Int {
-        return -1
+        val circuits = boxes.map { mutableListOf(it) }
+        val max3circuits = circuits.sortedBy { it.size }.take(3)
+        return max3circuits.fold(1) { acc, i -> acc * i.size }
     }
 
     fun solvePart2(): Int {
-        return -1
+        return UNKNOWN_VALUE
     }
 }
--- src/test/kotlin/year_2025/Day08Test.kt
+++ src/test/kotlin/year_2025/Day08Test.kt
@@ -8,17 +8,37 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-@DisplayName("Day 8")
+@DisplayName("Day 8: Playground")
 class Day08Test {
     companion object {
         val testInput = """
+            162,817,812
+            57,618,57
+            906,360,560
+            592,479,940
+            352,342,300
+            466,668,158
+            542,29,236
+            431,825,988
+            739,650,466
+            52,470,668
+            216,146,977
+            819,987,18
+            117,168,530
+            805,96,715
+            346,949,466
+            970,615,88
+            941,993,340
+            862,61,35
+            984,92,344
+            425,690,689
         """.trimIndent()
 
         val realInput = TaskData(2025, 8).asString()
 
         @JvmStatic
         fun part1Data() = listOf(
-            arguments(testInput, UNKNOWN_VALUE),
+            arguments(testInput, 40),
             arguments(realInput, UNKNOWN_VALUE)
         )
 

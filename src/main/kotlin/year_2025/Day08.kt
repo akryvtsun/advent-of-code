bb810b011753c5a59ab89f372194b9db10cdebcf
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-08 10:41:03 +0200

    refactoring

--- src/main/kotlin/year_2025/Day08.kt
+++ src/main/kotlin/year_2025/Day08.kt
@@ -21,73 +21,60 @@ class Day08(input: String) {
             Point3d(x, y, z)
         }
 
-    fun solvePart1(pairs: Int): Int {
-        // generate boxes pairs sorted by distance
-        val dists =
-            permutations(boxes)
-                .sortedBy { (b1, b2) -> b1.distanceTo(b2) }
+    // generate boxes pairs sorted by distance
+    val distances =
+        permutations(boxes)
+            .sortedBy { (b1, b2) -> b1.distanceTo(b2) }
+
+    fun solvePart1(pairsLimit: Int): Int {
+        val limitedDists = distances.take(pairsLimit)
 
         // merge pairs into circuit sets
-        val shortest10dists = dists.take(pairs)
-        val init = boxes.map { setOf(it) }.toMutableList()
-        val circuits = shortest10dists
-            .fold(init) { acc, pair ->
-                val addSet = mutableSetOf<Point3d>()
+        val circuits = boxes.map { setOf(it) }.toMutableList()
+        for (pair in limitedDists) {
+            val mergedCircuit = mutableSetOf<Point3d>()
 
-                val first = acc.firstOrNull { pair.first in it }
-                first?.let {
-                    acc.remove(it)
-                    addSet.addAll(it)
+            fun process(box: Point3d) {
+                val oldCircuit = circuits.firstOrNull { box in it }
+                oldCircuit?.let {
+                    circuits.remove(it)
+                    mergedCircuit.addAll(it)
                 }
-
-                val second = acc.firstOrNull { pair.second in it }
-                second?.let {
-                    acc.remove(it)
-                    addSet.addAll(it)
-                }
-
-                if (addSet.isNotEmpty()) acc.add(addSet)
-
-                acc
             }
+
+            process(pair.first)
+            process(pair.second)
+
+            if (mergedCircuit.isNotEmpty()) circuits.add(mergedCircuit)
+        }
 
         val max3circuits = circuits.sortedByDescending { it.size }.take(3)
         return max3circuits.fold(1) { acc, i -> acc * i.size }
     }
 
     fun solvePart2(): Long {
-        // generate boxes pairs sorted by distance
-        val dists =
-            permutations(boxes)
-                .sortedBy { (b1, b2) -> b1.distanceTo(b2) }
+        // merge pairs into circuit sets till all boxes joined
+        val circuits = boxes.map { setOf(it) }.toMutableList()
+        for (pair in distances) {
+            val mergedCircuit = mutableSetOf<Point3d>()
 
-        // merge pairs into circuit sets
-        val init = boxes.map { setOf(it) }.toMutableList()
-        var res: Long? = null
-        val circuits = dists
-            .fold(init) { acc, pair ->
-                val addSet = mutableSetOf<Point3d>()
-
-                val first = acc.firstOrNull { pair.first in it }
-                first?.let {
-                    acc.remove(it)
-                    addSet.addAll(it)
+            fun process(box: Point3d) {
+                val oldCircuit = circuits.firstOrNull { box in it }
+                oldCircuit?.let {
+                    circuits.remove(it)
+                    mergedCircuit.addAll(it)
                 }
-
-                val second = acc.firstOrNull { pair.second in it }
-                second?.let {
-                    acc.remove(it)
-                    addSet.addAll(it)
-                }
-
-                if (addSet.isNotEmpty()) acc.add(addSet)
-
-                if (acc.size == 1 && res == null) {
-                    res = pair.first.x.toLong() * pair.second.x.toLong()
-                }
-
-                acc
             }
-        return res!!
+
+            process(pair.first)
+            process(pair.second)
+
+            if (mergedCircuit.isNotEmpty()) circuits.add(mergedCircuit)
+
+            if (circuits.size == 1) {
+                return pair.first.x.toLong() * pair.second.x.toLong()
+            }
+        }
+        throw IllegalStateException("Not reachable")
     }
 }

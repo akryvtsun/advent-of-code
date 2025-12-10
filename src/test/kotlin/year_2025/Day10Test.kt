469ab6aec4280fbcd68cff15a826d6e13fc563d6
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-10 07:08:24 +0200

    year 2025, day 10 init

--- src/test/kotlin/year_2025/Day10Test.kt
+++ src/test/kotlin/year_2025/Day10Test.kt
@@ -8,17 +8,20 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-@DisplayName("Day 10")
+@DisplayName("Day 10: Factory")
 class Day10Test {
     companion object {
         val testInput = """
+            [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+            [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+            [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
         """.trimIndent()
 
         val realInput = TaskData(2025, 10).asString()
 
         @JvmStatic
         fun part1Data() = listOf(
-            arguments(testInput, UNKNOWN_VALUE),
+            arguments(testInput, 7),
             arguments(realInput, UNKNOWN_VALUE)
         )
 

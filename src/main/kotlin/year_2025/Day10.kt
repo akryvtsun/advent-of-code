985c2ab95721d453f6c4ebe31df451bf5382b27f
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-11 06:46:47 +0200

    year 2025, day 10, part 2 init

--- src/main/kotlin/year_2025/Day10.kt
+++ src/main/kotlin/year_2025/Day10.kt
@@ -1,11 +1,15 @@
 package year_2025
 
-import UNKNOWN_VALUE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import java.util.BitSet
+import kotlin.time.measureTimedValue
 
 class Day10(input: String) {
 
-    data class Machine(val lights: BitSet, val buttons: List<BitSet>/*, val joltage: List<Int>*/)
+    data class Machine(val lights: BitSet, val buttons: List<BitSet>, val joltage: List<Int>)
 
     private fun lights(data: String): BitSet {
         val result = BitSet()
@@ -19,23 +23,28 @@ class Day10(input: String) {
             it.trim('(', ')')
                 .split(',')
                 .map(String::toInt)
-                .forEach { i -> result.set(i) }
+                .forEach(result::set)
             result
         }
+
+    private fun joltage(data: String) =
+        data.trim('{', '}')
+            .split(',')
+            .map(String::toInt)
 
     val factory = input.lines()
         .map { line ->
             val parts = line.split(" ")
             Machine(
                 lights(parts.first()),
-                buttons(parts.subList(1, parts.size - 1))
-                // parts.last()
+                buttons(parts.subList(1, parts.size - 1)),
+                joltage(parts.last())
             )
         }
 
     fun findMinPresses(m: Machine): Int {
 
-        data class State(val lights: BitSet, val count: Int, val past: BitSet? = null)
+        data class State(val lights: BitSet, val count: Int, val pastButton: BitSet? = null)
 
         val states = ArrayDeque<State>()
         states.add(State(BitSet(), 0))
@@ -46,21 +55,56 @@ class Day10(input: String) {
                 return current.count
 
             for (button in m.buttons) {
+                if (button == current.pastButton) continue
                 val newLights = BitSet.valueOf(current.lights.toLongArray())
-                if (button == current.past) continue
                 newLights.xor(button)
                 val newState = State(newLights, current.count + 1, button)
                 states.add(newState)
             }
         }
-
-        error("unreachable point")
     }
 
     fun solvePart1(): Int =
         factory.sumOf { findMinPresses(it) }
 
-    fun solvePart2(): Int {
-        return UNKNOWN_VALUE
+    fun solvePart2(): Int = runBlocking(Dispatchers.IO) {
+        factory.mapIndexed { index, machine ->
+            async {
+                println("Computing $index")
+                val (value, duration) = measureTimedValue { findMinPresses2(machine) }
+                println("...completed $index with time $duration")
+                value
+            }
+        }
+            .awaitAll()
+            .sum()
+    }
+
+    fun findMinPresses2(m: Machine): Int {
+        val init = List(m.joltage.size) { 0 }
+        val keys = ArrayDeque<List<Int>>()
+        keys.add(init)
+        val states = mutableMapOf(init to 0)
+
+        while (true) {
+            val current = keys.removeFirst()
+            val count = states.remove(current)!!
+            if (current == m.joltage) {
+                return count
+            }
+            for (button in m.buttons) {
+                val newJoltage = current.toMutableList()
+                // button.stream().forEach { newJoltage[it]++ }
+                var idx = button.nextSetBit(0)
+                while (idx >= 0) {
+                    newJoltage[idx]++
+                    idx = button.nextSetBit(idx + 1)
+                }
+                if (newJoltage in states) continue
+                if ((0 until m.joltage.size).any { newJoltage[it] > m.joltage[it] }) continue
+                keys.add(newJoltage)
+                states[newJoltage] = count + 1
+            }
+        }
     }
 }
--- src/test/kotlin/year_2025/Day10Test.kt
+++ src/test/kotlin/year_2025/Day10Test.kt
@@ -1,7 +1,6 @@
 package year_2025
 
 import TaskData
-import UNKNOWN_VALUE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
@@ -17,6 +16,45 @@ class Day10Test {
             [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
         """.trimIndent()
 
+        // 0. A*(0,2) + B*(0,1) = 3
+        // 1. C*(1,3) + B*(0,1) = 5
+        // 2. D*(2) + E*(2,3) + A*(0,2) = 4
+        // 3. F*(3) + C*(1,3) + E*(2,3) = 7
+        // A+B+C+D+E+F -> min
+
+        // A = 1
+        // B = 2
+        // C = 3
+        // D =
+        // E = 3
+        // F = 1
+
+        // A + B = 3
+        // C + B = 5
+        // D + E + A = 4
+        // F + C + E = 7
+
+        // A = 3
+        // B = 0
+        // C = 5
+        // D + E = 1 -> E = 1 - D
+        // F + E = 2 -> F + 1 - D = 2 -> F - D = 1 -> F = 1 + D
+
+        // ===========
+
+        // 0. A(0,2,3,4)          + C(0,4) + D(0,1,2)              = 7
+        // 1.                                D(0,1,2) + E(1,2,3,4) = 5
+        // 2. A(0,2,3,4) + B(2,3)          + D(0,1,2) + E(1,2,3,4) = 12
+        // 3. A(0,2,3,4) + B(2,3)                     + E(1,2,3,4) = 7
+        // 4. A(0,2,3,4)          + C(0,4)            + E(1,2,3,4) = 2
+
+        // A + C + D = 7        v
+        // D + E = 5            V
+        // A + B + D + E = 12   v
+        // A + B + E = 7        V
+        // A + C + E = 2
+        // A+B+C+D+E -> ?
+
         val realInput = TaskData(2025, 10).asString()
 
         @JvmStatic
@@ -27,8 +65,8 @@ class Day10Test {
 
         @JvmStatic
         fun part2Data() = listOf(
-            arguments(testInput, UNKNOWN_VALUE),
-            arguments(realInput, UNKNOWN_VALUE)
+            arguments(testInput, 33),
+            //arguments(realInput, UNKNOWN_VALUE)
         )
     }
 

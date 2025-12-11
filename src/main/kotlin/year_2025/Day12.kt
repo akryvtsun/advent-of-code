f231fadce86360f4076a1d2ef1573c9b9f048d50
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-11 14:52:49 +0200

    day 12 preparation

---
+++ src/main/kotlin/year_2025/Day12.kt
@@ -0,0 +1,14 @@
+package year_2025
+
+import UNKNOWN_VALUE
+
+class Day12(input: String) {
+
+    fun solvePart1(): Int {
+        return UNKNOWN_VALUE
+    }
+
+    fun solvePart2(): Int {
+        return UNKNOWN_VALUE
+    }
+}

888c0164397fc5ce4f4ebbf6463483c2eae44bac
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-15 21:59:43 +0200

    day 17 refactoring

---
+++ src/main/kotlin/year_2024/Day17.kt
@@ -0,0 +1,141 @@
+package year_2024
+
+class Day17(private val input: String) {
+
+    data class Computer(val A: Int, val B: Int, val C: Int, val program: List<Int>)
+
+    fun transform(input: String): Computer {
+        val blocks = input.split("\n\n")
+        val regs = blocks[0].lines()
+        val A =  register(regs[0])
+        val B =  register(regs[1])
+        val C =  register(regs[2])
+        val program = blocks[1].substringAfterLast(" ").split(",").map { it.toInt() }
+        return Computer(A, B, C, program)
+    }
+
+    private fun register(data: String) = data.substringAfterLast(" ").toInt()
+
+    val config = transform(input)
+
+    fun solvePart1(): String {
+        val output = mutableListOf<Int>()
+
+        var A = config.A
+        var B = config.B
+        var C = config.C
+        var i = 0
+
+        fun getCombo(operand: Int): Int =
+            when (operand) {
+                in 0..3 -> operand
+                4 -> A
+                5 -> B
+                6 -> C
+                else -> throw IllegalArgumentException("Invalid operand")
+            }
+
+        while (i < config.program.size) {
+            val opcode = config.program[i++]
+            val operand = config.program[i++]
+            when (opcode) {
+                0 -> { // adv
+                    A /= (1 shl getCombo(operand))
+                }
+                1 -> { // bxl
+                    B = B xor operand
+                }
+                2 -> { // bst
+                    B = getCombo(operand) and 0b111
+                }
+                3 -> { // jnz
+                    if (A != 0) {
+                        i = operand
+                    }
+                }
+                4 -> { // bxc
+                    B = B xor C
+                }
+                5 -> { // out
+                    output += getCombo(operand) and 0b111
+                }
+                6 -> { // bdv
+                    B = A / (1 shl getCombo(operand))
+                }
+                7 -> { // cdv
+                    C = A / (1 shl getCombo(operand))
+                }
+                else -> throw IllegalArgumentException("Invalid operation")
+            }
+        }
+
+        return output.joinToString(separator = ",")
+    }
+
+    fun solvePart2(): Long {
+        val program = config.program
+
+        var A = 0L
+        var B = 0L
+        var C = 0L
+        var out = mutableListOf<Int>()
+
+        fun getCombo(operand: Int): Long =
+            when (operand) {
+                in 0..3 -> operand.toLong()
+                4 -> A
+                5 -> B
+                6 -> C
+                else -> throw IllegalArgumentException("Invalid operand")
+            }
+
+        fun execute(opcode: Int, operand: Int) {
+            when (opcode) {
+                0 -> { // adv
+                    A /= (1 shl getCombo(operand).toInt())
+                }
+                1 -> { // bxl
+                    B = B xor operand.toLong()
+                }
+                2 -> { // bst
+                    B = getCombo(operand) and 0b111
+                }
+                3 -> { // jnz
+                    // IGNORE
+                }
+                4 -> { // bxc
+                    B = B xor C
+                }
+                5 -> { // out
+                    out.addFirst((getCombo(operand) and 0b111).toInt())
+                }
+                6 -> { // bdv
+                    B = A / (1 shl getCombo(operand).toInt())
+                }
+                7 -> { // cdv
+                    C = A / (1 shl getCombo(operand).toInt())
+                }
+                else -> throw IllegalArgumentException("Invalid operation")
+            }
+        }
+
+        val pq = ArrayDeque<Pair<Long, List<Int>>>()
+        pq.add(Pair(0, emptyList()))
+
+        while (true) {
+            val (potentialA, output) = pq.removeFirst()
+            if (output == program) return potentialA
+
+            val left = potentialA * 8
+            val right = (potentialA+1) * 8 - 1
+            for (num in left..right) {
+                A = num
+                out = output.toMutableList()
+                program.windowed(2,2).forEach { execute(it[0], it[1]) }
+                if (num != 0L && out == program.subList(program.size - out.size, program.size)) {
+                    pq.addLast(num to out)
+                }
+            }
+        }
+    }
+}
--- src/test/kotlin/year_2024/Day17Test.kt
+++ src/test/kotlin/year_2024/Day17Test.kt
@@ -1,104 +1,86 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import taskData
-import year_2024.day_17.Computer
-import year_2024.day_17.Task1
-import year_2024.day_17.Task2
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("Day 17: Chronospatial Computer")
 class Day17Test {
+    companion object {
+        val testInput1 = """
+            Register A: 729
+            Register B: 0
+            Register C: 0
+            
+            Program: 0,1,5,4,3,0
+        """.trimIndent()
 
-    fun transform(input: String): Computer {
-        val blocks = input.split("\n\n")
-        val regs = blocks[0].lines()
-        val A =  register(regs[0])
-        val B =  register(regs[1])
-        val C =  register(regs[2])
-        val program = blocks[1].substringAfterLast(" ").split(",").map { it.toInt() }
-        return Computer(A, B, C, program)
+        val testInput2 = """
+            Register A: 117440
+            Register B: 0
+            Register C: 0
+            
+            Program: 0,3,5,4,3,0
+        """.trimIndent()
+
+        val caseInput2 = """
+            Register A: 10
+            Register B: 0
+            Register C: 0
+            
+            Program: 5,0,5,1,5,4
+        """.trimIndent()
+
+        val caseInput3 = """
+            Register A: 2024
+            Register B: 0
+            Register C: 0
+            
+            Program: 0,1,5,4,3,0
+        """.trimIndent()
+
+        val realInput = TaskData(2024, 17).asString()
+
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput1, "4,6,3,5,6,3,5,2,1,0"),
+            arguments(testInput2, "0,3,5,4,3,0"),
+            arguments(caseInput2, "0,1,2"),
+            arguments(caseInput3, "4,2,5,6,7,7,7,7,3,1,0"),
+            arguments(realInput, "7,5,4,3,4,5,3,4,6")
+        )
+
+        val testInput_2 = """
+            Register A: -1
+            Register B: -1
+            Register C: -1
+            
+            Program: 0,3,5,4,3,0
+        """.trimIndent()
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(testInput_2, 117440),
+            arguments(realInput, 164278899142333)
+        )
     }
 
-    private fun register(data: String) = data.substringAfterLast(" ").toInt()
-
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @org.junit.jupiter.api.Test
-        fun `should successfully pass task example`() {
-            val config = """
-                Register A: 729
-                Register B: 0
-                Register C: 0
-                
-                Program: 0,1,5,4,3,0
-            """.trimIndent()
-            val input = transform(config)
-            assertEquals("4,6,3,5,6,3,5,2,1,0", Task1.solve(input).second)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `should successfully pass task 2 example`() {
-            val config = """
-                Register A: 117440
-                Register B: 0
-                Register C: 0
-                
-                Program: 0,3,5,4,3,0
-            """.trimIndent()
-            val input = transform(config)
-            assertEquals("0,3,5,4,3,0", Task1.solve(input).second)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `test case 1`() {
-            assertEquals(1, Task1.solve(Computer(0, 0, 9, listOf(2, 6))).first.B)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `test case 2`() {
-            assertEquals("0,1,2", Task1.solve(Computer(10, 0, 0, listOf(5, 0, 5, 1, 5, 4))).second)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `test case 3`() {
-            assertEquals("4,2,5,6,7,7,7,7,3,1,0", Task1.solve(Computer(2024, 0, 0, listOf(0, 1, 5, 4, 3, 0))).second)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `test case 4`() {
-            assertEquals(26, Task1.solve(Computer(0, 29, 0, listOf(1, 7))).first.B)
-        }
-
-        @org.junit.jupiter.api.Test
-        fun `test case 5`() {
-            assertEquals(44354, Task1.solve(Computer(0, 2024, 43690, listOf(4, 0))).first.B)
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val map = taskData(2024, 17).readText()
-            val input = transform(map)
-            assertEquals("7,5,4,3,4,5,3,4,6", Task1.solve(input).second)
-        }
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: String) {
+        assertThat(
+            Day17(input).solvePart1()
+        ).isEqualTo(result)
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Matches example`() {
-            assertEquals(117440, Task2.solve(listOf(0, 3, 5, 4, 3, 0)))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val map = taskData(2024, 17).readText()
-            val input = transform(map)
-            assertEquals(164278899142333, Task2.solve(input.program))
-        }
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, result: Long) {
+        assertThat(
+            Day17(input).solvePart2()
+        ).isEqualTo(result)
     }
 }

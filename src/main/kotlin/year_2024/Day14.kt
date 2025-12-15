4691c822372341f582dc1de04033ad5076527afd
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-15 19:50:32 +0200

    year 2024, day 14 refactoring

---
+++ src/main/kotlin/year_2024/Day14.kt
@@ -0,0 +1,126 @@
+package year_2024
+
+class Day14(
+    val input: String,
+    val height: Int, val width: Int
+) {
+
+    data class Robot(val coord: Point, val delta: Point)
+
+    fun getPoint(data: String): Pair<Int, Int> {
+        val (x, y) = """[pv]=(.+),(.+)""".toRegex().find(data)!!.destructured
+        return x.toInt() to y.toInt()
+    }
+
+    fun transform(input: String) = input.lines()
+        .map { line ->
+            val (c, d) = line.split(" ")
+            val coord = getPoint(c).let { Point(it.second, it.first) }
+            val delta = getPoint(d).let { Point(it.second, it.first) }
+            Robot(coord, delta)
+        }
+
+    fun solvePart1(): Int {
+        val data = transform(input)
+        return solve1(height, width, data)
+    }
+
+    fun solve1(height: Int, width: Int, robots: List<Robot>): Int {
+
+        fun roundMove(value: Int, delta: Int, border: Int): Int {
+            val newValue = (value + delta) % border
+            return if (newValue < 0) border + newValue else newValue
+        }
+
+        fun move(unit: Robot, rounds: Int): Point {
+            var curY = unit.coord.y
+            var curX = unit.coord.x
+            repeat(rounds) {
+                curY = roundMove(curY, unit.delta.y, height)
+                curX = roundMove(curX, unit.delta.x, width)
+            }
+            return Point(curY, curX)
+        }
+
+        fun quadrant(c: Point): Int {
+            val y0 = height/2
+            val x0 = width/2
+            return if (c.y < y0) {
+                if (c.x < x0) {
+                    1
+                } else if (c.x > x0) {
+                    2
+                }
+                else {
+                    0
+                }
+            } else if (c.y > y0) {
+                if (c.x < x0) {
+                    3
+                } else if (c.x > x0) {
+                    4
+                }
+                else {
+                    0
+                }
+            }
+            else {
+                0
+            }
+        }
+
+        val newCoords = robots.map { move(it, 100) }
+        val securityMap = newCoords.map { it to quadrant(it) }.groupBy({ it.second }, { it.first })
+        return securityMap
+            .filter { it.key != 0 }
+            .map { it.value.size }
+            .fold(1) { acc, num -> acc * num }
+    }
+
+    fun solvePart2(): Int {
+        val data = transform(input)
+        return solve2(height, width, data)
+    }
+
+    fun solve2(height: Int, width: Int, robots: List<Robot>): Int {
+
+        fun roundMove(value: Int, delta: Int, border: Int): Int {
+            val newValue = (value + delta) % border
+            return if (newValue < 0) border + newValue else newValue
+        }
+
+        fun Robot.doStep(): Robot {
+            var newY = roundMove(this.coord.y, this.delta.y, height)
+            var newX = roundMove(this.coord.x, this.delta.x, width)
+            return Robot(Point(newY, newX), this.delta)
+        }
+
+        fun checkTree(robots: List<Robot>): Boolean {
+            for (y in 0..height) {
+                var line = StringBuilder()
+                for (x in 0..width) {
+                    val char = if (robots.any { it.coord == Point(y, x) }) '#' else ' '
+                    line.append(char)
+                }
+                val found = line.indexOf("########") != -1
+                if (found) return true
+            }
+            return false
+        }
+
+        val treeState = generateSequence(0 to robots) { state ->
+            state.first + 1 to state.second.map { it.doStep() }
+        }
+            .first { checkTree(it.second) }
+        println(">>>>>>>>>>>>>>>>> STEP: ${treeState.first} >>>>>>>>>>>>>>>>>")
+        for (y in 0..height) {
+            var line = StringBuilder()
+            for (x in 0..width) {
+                val char = if (treeState.second.any { it.coord == Point(y, x) }) '#' else ' '
+                line.append(char)
+            }
+            println(line)
+        }
+        return treeState.first
+    }
+}
--- src/test/kotlin/year_2024/Day14Test.kt
+++ src/test/kotlin/year_2024/Day14Test.kt
@@ -1,65 +1,57 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import taskData
-import year_2024.day_14.Robot
-import year_2024.day_14.Task1
-import year_2024.day_14.Task2
-import year_2024.day_14.getPoint
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("Day 14: Restroom Redoubt")
 class Day14Test {
+    companion object {
+        val testInput = """
+            p=0,4 v=3,-3
+            p=6,3 v=-1,-3
+            p=10,3 v=-1,2
+            p=2,0 v=2,-1
+            p=0,0 v=1,3
+            p=3,0 v=-2,-2
+            p=7,6 v=-1,-3
+            p=3,0 v=-1,-2
+            p=9,3 v=2,3
+            p=7,3 v=-1,2
+            p=2,4 v=2,-3
+            p=9,5 v=-3,-3
+        """.trimIndent()
 
-    fun transform(input: String) = input.lines()
-        .map { line ->
-            val (c, d) = line.split(" ")
-            val coord = getPoint(c).let { Point(it.second, it.first) }
-            val delta = getPoint(d).let { Point(it.second, it.first) }
-            Robot(coord, delta)
-        }
+        val realInput = TaskData(2024, 14).asString()
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `Matches example`() {
-            val config = """
-                p=0,4 v=3,-3
-                p=6,3 v=-1,-3
-                p=10,3 v=-1,2
-                p=2,0 v=2,-1
-                p=0,0 v=1,3
-                p=3,0 v=-2,-2
-                p=7,6 v=-1,-3
-                p=3,0 v=-1,-2
-                p=9,3 v=2,3
-                p=7,3 v=-1,2
-                p=2,4 v=2,-3
-                p=9,5 v=-3,-3
-            """.trimIndent()
-            val input = transform(config)
-            assertEquals(12, Task1.solve(7, 11, input))
-        }
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, 7, 11, 12),
+            arguments(realInput, 103, 101, 230461440)
+        )
 
-        @Test
-        fun `Actual answer`() {
-            val map = taskData(2024, 14).readText()
-            val input = transform(map)
-            assertEquals(230461440, Task1.solve(103, 101, input))
-        }
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(realInput, 103, 101, 6668)
+        )
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Actual answer`() {
-            val map = taskData(2024, 14).readText()
-            val input = transform(map)
-            assertEquals(6668, Task2.solve(103, 101, input))
-        }
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, height: Int, width: Int, result: Int) {
+        assertThat(
+            Day14(input, height, width).solvePart1()
+        ).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String, height: Int, width: Int, result: Long) {
+        assertThat(
+            Day14(input, height, width).solvePart2()
+        ).isEqualTo(result)
     }
 }

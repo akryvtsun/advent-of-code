6dd58759152601b06d46fe4e961dce720941fa28
Andriy Kryvtsun <kontiky@gmail.com>
2025-12-16 13:34:50 +0200

    day 21 refactoring

---
+++ src/main/kotlin/year_2024/Day21.kt
@@ -0,0 +1,167 @@
+package year_2024
+
+import java.util.PriorityQueue
+
+typealias Keypad = Map<Char, Point>
+typealias Grid<T> = Map<Point, T>
+
+class Day21(val input: String) {
+
+    companion object {
+        val NORTH = Point(-1, 0)
+        val EAST = Point(0, 1)
+        val SOUTH = Point(1, 0)
+        val WEST = Point(0, -1)
+
+        enum class Direction(val c: Char, val delta: Point) {
+            UP('^', Point(-1, 0)),
+            RIGHT('>', Point(0, 1)),
+            DOWN('v', Point(1, 0)),
+            LEFT('<', Point(0, -1));
+        }
+
+        val numericKeypad = buildMap {
+            put('7', Point(0, 0)); put('8', Point(0, 1)); put('9', Point(0, 2))
+            put('4', Point(1, 0)); put('5', Point(1, 1)); put('6', Point(1, 2))
+            put('1', Point(2, 0)); put('2', Point(2, 1)); put('3', Point(2, 2))
+            put('0', Point(3, 1)); put('A', Point(3, 2))
+        }
+
+        val directionalKeypad = buildMap {
+            put('^', Point(0, 1)); put('A', Point(0, 2))
+            put('<', Point(1, 0)); put('v', Point(1, 1)); put('>', Point(1, 2))
+        }
+
+        private fun findPaths(begin: Point, end: Point, keypad: Map<Char, Point>): Set<String> {
+            val paths = mutableSetOf<String>()
+            var minLength = Int.MAX_VALUE
+
+            val queue = PriorityQueue<Pair<Point, String>>(compareBy { it.second.length })
+            queue.add(begin to "")
+            while (true) {
+                val (pos, path) = queue.remove()
+                if (pos == end) {
+                    if (minLength >= path.length) {
+                        minLength = path.length
+                        paths += path + 'A'
+                    }
+                    else
+                        return paths
+                }
+                val next = Direction.entries
+                    .map { pos + it.delta to path + it.c }
+                    .filter { (nextPos, _) -> keypad.containsValue(nextPos) }
+                queue.addAll(next)
+            }
+        }
+
+        private fun cartesian(lists: List<Set<String>>): List<List<String>> {
+            return lists.fold(listOf(listOf())) { acc, set ->
+                acc.flatMap { combination ->
+                    set.map { element -> combination + element }
+                }
+            }
+        }
+
+        private fun keypadPaths(code: String, keypad: Map<Char, Point>): Set<String> {
+            val paths = "A$code"
+                .zipWithNext()
+                .map { (start, stop) ->
+                    val begin = keypad[start]!!
+                    val end = keypad[stop]!!
+                    findPaths(begin, end, keypad)
+                }
+            return cartesian(paths).map { it.joinToString(separator = "") }.toSet()
+        }
+
+        fun keypadPaths(codes: Set<String>, keypad: Map<Char, Point>): Set<String> {
+            val pq = PriorityQueue<String>(compareBy { it.length })
+            pq.addAll(codes.flatMap { keypadPaths(it, keypad) })
+            val minPathLength = pq.element().length
+            return pq.filter { it.length == minPathLength }.toSet()
+        }
+
+        fun shortestPath(code: String): String {
+            var paths = keypadPaths(setOf(code), numericKeypad)
+            repeat(2) {
+                paths = keypadPaths(paths, directionalKeypad)
+            }
+            return paths.first()
+        }
+    }
+
+    val pads = input.lines()
+
+    private fun complexity(pad: String, seq: String) =
+        pad.substringBefore('A').toInt() * seq.length
+
+    fun solvePart1(): Int {
+        return pads.sumOf { complexity(it, shortestPath(it)) }
+    }
+
+    val numbersPad = """
+            789
+            456
+            123
+            -0A
+        """.trimIndent().toKeypad()
+
+    val directionPad = """
+            -^A
+            <v>
+        """.trimIndent().toKeypad()
+
+    fun String.toGrid(): Grid<Char> = lines().flatMapIndexed { y, line -> line.mapIndexed { x, c -> Point(y, x) to c } }.toMap()
+
+    fun String.toKeypad(): Keypad = toGrid().entries.associateBy({ it.value }, { it.key })
+
+    fun List<Point>.toCharacters() = map {
+        when (it) {
+            NORTH -> '^'
+            SOUTH -> 'v'
+            EAST -> '>'
+            WEST -> '<'
+            else -> error("invalid direction")
+        }
+    }.joinToString("", postfix = "A")
+
+    fun State.routes(keypad: Keypad): List<String> {
+        val start = keypad.getValue(from)
+        val goal = keypad.getValue(to)
+        val hole = keypad.getValue('-')
+        return generateSequence(listOf(start to emptyList<Point>())) { frontier ->
+            frontier.flatMap { (end, path) ->
+                when (end) {
+                    hole -> emptyList()
+                    else -> listOfNotNull(
+                        if (end.x > goal.x) end + WEST to path + WEST else null,
+                        if (end.x < goal.x) end + EAST to path + EAST else null,
+                        if (end.y > goal.y) end + NORTH to path + NORTH else null,
+                        if (end.y < goal.y) end + SOUTH to path + SOUTH else null
+                    )
+                }
+            }
+        }.takeWhile { it.isNotEmpty() }.last().map { (_, line) -> line.toCharacters() }
+    }
+
+    val memoization = mutableMapOf<State, Long>()
+
+    fun State.solve(first: Boolean): Long = memoization.getOrPut(this) {
+        val keypad = if (first) numbersPad else directionPad
+        val routes = routes(keypad)
+        when (depth) {
+            0 -> routes.first().length.toLong()
+            else -> routes.minOf { it.sizeOfCommands(depth - 1, false) }
+        }
+    }
+
+    data class State(val from: Char, val to: Char, val depth: Int)
+
+    fun String.sizeOfCommands(depth: Int, first: Boolean = true): Long = "A$this"
+        .zipWithNext { a, b -> State(a, b, depth).solve(first) }
+        .sum()
+
+    fun solvePart2(): Long {
+        return pads.sumOf { it.sizeOfCommands(25) * it.substringBefore('A').toInt() }
+    }
+}
--- src/test/kotlin/year_2024/Day21Test.kt
+++ src/test/kotlin/year_2024/Day21Test.kt
@@ -1,15 +1,14 @@
 package year_2024
 
+import TaskData
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
-import taskData
-import year_2024.day_21.Task1
-import year_2024.day_21.Task2
-import year_2024.day_21.directionalKeypad
-import year_2024.day_21.numericKeypad
+import year_2024.Day21.Companion.directionalKeypad
+import year_2024.Day21.Companion.numericKeypad
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -17,6 +16,16 @@ import kotlin.test.assertEquals
 class Day21Test {
 
     companion object {
+        val testInput = """
+            029A
+            980A
+            179A
+            456A
+            379A
+        """.trimIndent()
+
+        val realInput = TaskData(2024, 21).asString()
+
         @JvmStatic
         fun paths() = listOf(
             Arguments.of("029A", "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"),
@@ -25,64 +34,56 @@ class Day21Test {
             Arguments.of("456A", "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A"),
             Arguments.of("379A", "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"),
         )
+
+        @JvmStatic
+        fun part1Data() = listOf(
+            arguments(testInput, 126384),
+            arguments(realInput, 203814)
+        )
+
+        @JvmStatic
+        fun part2Data() = listOf(
+            arguments(realInput, 248566068436630)
+        )
     }
 
-    @Nested
-    @DisplayName("Part 1")
-    inner class Part1 {
-        @Test
-        fun `should successfully find path for 029A`() {
-            val path1 = "<A^A>^^AvvvA"
-            assertEquals(
-                path1.length,   // 12
-                Task1.keypadPaths(setOf("029A"), numericKeypad).first().length)
+    @Test
+    fun `should successfully find path for 029A`() {
+        val path1 = "<A^A>^^AvvvA"
+        assertEquals(
+            path1.length,   // 12
+            Day21.keypadPaths(setOf("029A"), numericKeypad).first().length)
 
-            val path2 = "v<<A>>^A<A>AvA<^AA>A<vAAA>^A"
-            assertEquals(
-                path2.length,   // 28
-                Task1.keypadPaths(setOf(path1), directionalKeypad).first().length)
+        val path2 = "v<<A>>^A<A>AvA<^AA>A<vAAA>^A"
+        assertEquals(
+            path2.length,   // 28
+            Day21.keypadPaths(setOf(path1), directionalKeypad).first().length)
 
-            val path3 = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
-            assertEquals(
-                path3.length,   // 68
-                Task1.keypadPaths(setOf(path2), directionalKeypad).first().length)
-        }
-
-        @ParameterizedTest
-        @MethodSource("year_2024.Day21Test#paths")
-        fun `should successfully find shortest path`(code: String, path: String) {
-            assertEquals(path.length, Task1.shortestPath(code).length)
-        }
-
-        @Test
-        fun `Matches example`() {
-            val input = """
-                029A
-                980A
-                179A
-                456A
-                379A
-            """.trimIndent()
-            val data = input.lines()
-            assertEquals(126384, Task1.solve(data))
-        }
-
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 21).readText()
-            val data = input.lines()
-            assertEquals(203814, Task1.solve(data))
-        }
+        val path3 = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
+        assertEquals(
+            path3.length,   // 68
+            Day21.keypadPaths(setOf(path2), directionalKeypad).first().length)
     }
 
-    @Nested
-    @DisplayName("Part 2")
-    inner class Part2 {
-        @Test
-        fun `Actual answer`() {
-            val input = taskData(2024, 21).readText()
-            val data = input.lines()
-            assertEquals(248566068436630, Task2.solve(data))
-        }
+    @ParameterizedTest
+    @MethodSource("paths")
+    fun `should successfully find shortest path`(code: String, path: String) {
+        assertEquals(path.length, Day21.shortestPath(code).length)
+    }
+
+    @ParameterizedTest
+    @MethodSource("part1Data")
+    fun part1Test(input: String, result: Int) {
+        assertThat(
+            Day21(input).solvePart1()
+        ).isEqualTo(result)
+    }
+
+    @ParameterizedTest
+    @MethodSource("part2Data")
+    fun part2Test(input: String,result: Long) {
+        assertThat(
+            Day21(input).solvePart2()
+        ).isEqualTo(result)
     }
 }
